05bb9b5e8c3160e29503b8b87f5e46da11bfbe2d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-04 14:31:31 +0200

    corrected padding logic in wavelets

--- wavelets.py
+++ wavelets.py
@@ -13,6 +13,11 @@ class WaveletDecimated(Wavelet2):
         self.im_shape = None
         self.big_dim = None
         self.diff = None
+        self.pad_seq = None
+
+    def pad_image(self, image):
+        image = np.pad(image, self.pad_seq, mode='constant')
+        return image
 
     def op(self, data):
         if self.im_shape is None:
@@ -21,23 +26,26 @@ class WaveletDecimated(Wavelet2):
                 if np.argmax(data.shape) == 0:
                     self.big_dim = 0
                     self.diff = data.shape[0] - data.shape[1]
-                    pad_seq = [(0, 0), (self.diff // 2, self.diff // 2)]
+                    self.pad_seq = [(0, 0), (self.diff // 2, self.diff // 2)]
                 else:
                     self.big_dim = 1
                     self.diff = data.shape[1] - data.shape[0]
-                    pad_seq = [(self.diff // 2, self.diff // 2), (0, 0)]
-                data = np.pad(data, pad_seq, mode='constant')
+                    self.pad_seq = [(self.diff // 2, self.diff // 2), (0, 0)]
         else:
             if self.im_shape != data.shape:
                 raise ValueError('Shape not corresponding')
+        if not is_square(data):
+            data = self.pad_image(data)
+        import pdb; pdb.set_trace()
         return super(WaveletDecimated, self).op(data)
 
     def adj_op(self, data):
         if self.im_shape is None:
             raise ValueError('Op not called first')
         im = super(WaveletDecimated, self).adj_op(data)
-        if self.big_dim == 0:
-            im = im[:, self.diff // 2: - self.diff // 2]
-        if self.big_dim == 1:
-            im = im[self.diff // 2: - self.diff // 2, :]
+        if self.big_dim is not None:
+            if self.big_dim == 0:
+                im = im[:, self.diff // 2: - self.diff // 2]
+            if self.big_dim == 1:
+                im = im[self.diff // 2: - self.diff // 2, :]
         return im
