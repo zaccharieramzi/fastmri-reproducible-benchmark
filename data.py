ea74562892e462bc8feaef558792751411616373
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-15 18:15:28 +0200

    added 3d gen

--- data.py
+++ data.py
@@ -69,3 +69,50 @@ def zero_filled_2d_generator(path, mode='training', batch_size=32, af=None):
                         zero_img_batch = np.array(current_batch_zero)
                         current_batch_zero = []
                         yield zero_img_batch
+
+
+def zero_filled_3d_generator(path, mode='training', batch_size=32, af=None):
+    train_modes = ('training', 'validation')
+    filenames = glob.glob(path + '*.h5')
+    while True:
+        current_batch_zero = []
+        current_batch = []
+        for i_file, filename in enumerate(filenames):
+            if mode in train_modes:
+                images, kspaces, mask = from_train_file_to_image_and_kspace(filename)
+                if af is not None:
+                    mask_af = len(mask) / sum(mask)
+                    if not(af == 4 and mask_af < 5.5 or af == 8 and mask_af > 8):
+                        continue
+                zero_filled_recs = list()
+                for kspace in kspaces:
+                    zero_filled_rec = zero_filled(kspace)
+                    zero_filled_recs.append(zero_filled_rec)
+                zero_filled_recs = np.array(zero_filled_recs)
+                zero_filled_recs = zero_filled_recs[..., None]
+                images = images[..., None]
+                current_batch_zero.append(zero_filled_recs)
+                current_batch.append(images)
+                if (i_file + 1) % batch_size == 0:
+                    zero_img_batch = np.array(current_batch_zero)
+                    img_batch = np.array(current_batch)
+                    current_batch = []
+                    current_batch_zero = []
+                    yield (zero_img_batch, img_batch)
+            else:
+                mask, kspaces = from_test_file_to_mask_and_kspace(filename)
+                if af is not None:
+                    mask_af = len(mask) / sum(mask)
+                    if not(af == 4 and mask_af < 5.5 or af == 8 and mask_af > 8):
+                        continue
+                zero_filled_recs = list()
+                for kspace in kspaces:
+                    zero_filled_rec = zero_filled(kspace)
+                    zero_filled_recs.append(zero_filled_rec)
+                zero_filled_recs = np.array(zero_filled_recs)
+                zero_filled_recs = zero_filled_recs[..., None]
+                current_batch_zero.append(zero_filled_recs)
+                if (i_file + 1) % batch_size == 0:
+                    zero_img_batch = np.array(current_batch_zero)
+                    current_batch_zero = []
+                    yield zero_img_batch
