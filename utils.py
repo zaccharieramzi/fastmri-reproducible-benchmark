008250258c7ea9d212916eb5727fa60d29d774d1
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-18 15:47:16 +0200

    added normalizing functions

--- utils.py
+++ utils.py
@@ -36,6 +36,41 @@ def gen_mask(kspace, accel_factor=8):
     mask = mask.reshape(*mask_shape)
     return mask
 
+# taken from https://github.com/facebookresearch/fastMRI/
+def normalize(data, mean, stddev, eps=0.):
+    """
+    Normalize the given tensor using:
+        (data - mean) / (stddev + eps)
+
+    Args:
+        data (torch.Tensor): Input data to be normalized
+        mean (float): Mean value
+        stddev (float): Standard deviation
+        eps (float): Added to stddev to prevent dividing by zero
+
+    Returns:
+        torch.Tensor: Normalized tensor
+    """
+    return (data - mean) / (stddev + eps)
+
+
+def normalize_instance(data, eps=0.):
+    """
+        Normalize the given tensor using:
+            (data - mean) / (stddev + eps)
+        where mean and stddev are computed from the data itself.
+
+        Args:
+            data (torch.Tensor): Input data to be normalized
+            eps (float): Added to stddev to prevent dividing by zero
+
+        Returns:
+            torch.Tensor: Normalized tensor
+        """
+    mean = data.mean()
+    std = data.std()
+    return normalize(data, mean, std, eps), mean, std
+
 # TODO have the same in 3D
 def keras_psnr(y_true, y_pred):
     max_pixel = K.max(y_true)
