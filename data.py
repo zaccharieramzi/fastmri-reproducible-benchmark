c75dad6e2f1bf336e04ba0d941cec15bffc4136e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-18 18:33:24 +0200

    added zero padded data seq

--- data.py
+++ data.py
@@ -28,6 +28,15 @@ def from_file_to_kspace(filename):
         kspaces = h5_obj['kspace'][()]
         return kspaces
 
+def fft(image):
+    fourier_op = FFT2(np.ones_like(image))
+    kspace = fourier_op.op(image)
+    return kspace
+
+def ifft(kspace):
+    fourier_op = FFT2(np.ones_like(kspace))
+    image = fourier_op.adj_op(kspace)
+    return image
 
 def zero_filled(kspace):
     fourier_op = FFT2(np.ones_like(kspace))
@@ -36,7 +45,7 @@ def zero_filled(kspace):
     return im_cropped
 
 
-class ZeroFilled2DSequence(Sequence):
+class fastMRI2DSequence(Sequence):
     train_modes = ('training', 'validation')
 
     def __init__(self, path, mode='training', af=4, norm=False):
@@ -71,6 +80,65 @@ class ZeroFilled2DSequence(Sequence):
             return self.get_item_test(filename)
 
 
+    def get_item_train(self, filename):
+        pass
+
+    def get_item_test(self, filename):
+        pass
+
+
+class ZeroPadded2DSequence(fastMRI2DSequence):
+    pad = 644
+    def get_item_train(self, filename):
+        images, kspaces = from_train_file_to_image_and_kspace(filename)
+        mask = gen_mask(kspaces[0], accel_factor=self.af)
+        fourier_mask = np.repeat(mask.astype(np.float), kspaces[0].shape[0], axis=0)
+        img_batch = list()
+        z_kspace_batch = list()
+        to_pad = type(self).pad - kspaces.shape[2]
+        pad_seq = [(0, 0), (to_pad // 2, to_pad // 2)]
+        for kspace, image in zip(kspaces, images):
+            zero_filled_rec = ifft(kspace * fourier_mask)
+            if self.norm:
+                zero_filled_rec, mean, std = normalize_instance(zero_filled_rec, eps=1e-11)
+                image = normalize(image, mean, std, eps=1e-11)
+            zero_filled_rec = np.pad(zero_filled_rec, pad_seq, mode='constant')
+            z_kspace = fft(zero_filled_rec)
+            z_kspace = z_kspace[:, :, None]
+            z_kspace_batch.append(z_kspace)
+            image = np.pad(image, pad_seq, mode='constant')
+            image = image[..., None]
+            img_batch.append(image)
+        z_kspace_batch = np.array(z_kspace_batch)
+        img_batch = np.array(img_batch)
+        return (z_kspace_batch, img_batch)
+
+
+    def get_item_test(self, filename):
+        _, kspaces = from_test_file_to_mask_and_kspace(filename)
+        z_kspace_batch = list()
+        means = list()
+        stddevs = list()
+        to_pad = type(self).pad - kspaces.shape[2]
+        pad_seq = [(0, 0), (to_pad // 2, to_pad // 2)]
+        for kspace in kspaces:
+            zero_filled_rec = ifft(kspace)
+            if self.norm:
+                zero_filled_rec, mean, std = normalize_instance(zero_filled_rec, eps=1e-11)
+                means.append(mean)
+                stddevs.append(std)
+            zero_filled_rec = np.pad(zero_filled_rec, pad_seq, mode='constant')
+            z_kspace = fft(zero_filled_rec)
+            z_kspace = z_kspace[:, :, None]
+            z_kspace_batch.append(z_kspace)
+        z_kspace_batch = np.array(z_kspace_batch)
+        if self.norm:
+            return z_kspace_batch, means, stddevs
+        else:
+            return z_kspace_batch
+
+
+class ZeroFilled2DSequence(fastMRI2DSequence):
     def get_item_train(self, filename):
         images, kspaces = from_train_file_to_image_and_kspace(filename)
         mask = gen_mask(kspaces[0], accel_factor=self.af)
