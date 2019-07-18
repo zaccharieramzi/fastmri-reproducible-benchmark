0c0c676f29157e4b1cd15a631ccf6b803a2be01f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-18 15:49:30 +0200

    added the evaluation module for clearer evaluation and closer to fb

---
+++ evaluate.py
@@ -0,0 +1,71 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+import h5py
+import numpy as np
+from runstats import Statistics
+from skimage.measure import compare_psnr, compare_ssim
+
+
+def mse(gt, pred):
+    """ Compute Mean Squared Error (MSE) """
+    return np.mean((gt - pred) ** 2)
+
+
+def nmse(gt, pred):
+    """ Compute Normalized Mean Squared Error (NMSE) """
+    return np.linalg.norm(gt - pred) ** 2 / np.linalg.norm(gt) ** 2
+
+
+def psnr(gt, pred):
+    """ Compute Peak Signal to Noise Ratio metric (PSNR) """
+    return compare_psnr(gt, pred, data_range=gt.max())
+
+
+def ssim(gt, pred):
+    """ Compute Structural Similarity Index Metric (SSIM). """
+    return compare_ssim(
+        gt.transpose(1, 2, 0), pred.transpose(1, 2, 0), multichannel=True, data_range=gt.max()
+    )
+
+
+METRIC_FUNCS = dict(
+    PSNR=psnr,
+    SSIM=ssim,
+)
+
+
+class Metrics:
+    """
+    Maintains running statistics for a given collection of metrics.
+    """
+
+    def __init__(self, metric_funcs):
+        self.metrics = {
+            metric: Statistics() for metric in metric_funcs
+        }
+
+    def push(self, target, recons):
+        for metric, func in METRIC_FUNCS.items():
+            self.metrics[metric].push(func(target, recons))
+
+    def means(self):
+        return {
+            metric: stat.mean() for metric, stat in self.metrics.items()
+        }
+
+    def stddevs(self):
+        return {
+            metric: stat.stddev() for metric, stat in self.metrics.items()
+        }
+
+    def __repr__(self):
+        means = self.means()
+        stddevs = self.stddevs()
+        metric_names = sorted(list(means))
+        return ' '.join(
+            f'{name} = {means[name]:.4g} +/- {2 * stddevs[name]:.4g}' for name in metric_names
+        )
