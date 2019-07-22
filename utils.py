8d2d81e4646c9b0f705e1d35501e192ba504104b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-22 10:37:13 +0200

    first try for the custom batch metrics

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import random
 
 from keras import backend as K
+from keras.callbacks import Callback
 import numpy as np
 import tensorflow as tf
 
@@ -79,3 +80,35 @@ def keras_psnr(y_true, y_pred):
 def keras_ssim(y_true, y_pred):
     max_pixel = K.max(y_true)
     return tf.image.ssim(y_true, y_pred, max_pixel)
+
+
+class VolumeMetrics(Callback):
+    def __init__(self, verbose=0):
+        super(VolumeMetrics, self).__init__()
+        self.verbose = verbose
+
+    def on_train_begin(self):
+        self.train_psnrs = list()
+        self.train_ssims = list()
+        self.val_psnrs = list()
+        self.val_ssims = list()
+
+    def on_batch_end(self, batch, logs=None):
+        y_pred = self.model.predict(self.model.training_data[0])
+        psnr = keras_psnr(self.model.training_data[1], y_pred)
+        self.train_psnrs.append(psnr)
+        ssim = keras_ssim(self.model.training_data[1], y_pred)
+        self.train_ssims.append(ssim)
+        if self.verbose > 1:
+            print('Training PSNR at batch {batch}: {self.train_psnrs[-1]}')
+            print('Training SSIM at batch {batch}: {self.train_ssims[-1]}')
+
+    def on_epoch_end(self, epoch, logs=None):
+        y_pred = self.model.predict(self.model.validation_data[0])
+        psnr = keras_psnr(self.model.validation_data[1], y_pred)
+        self.val_psnrs.append(psnr)
+        ssim = keras_ssim(self.model.validation_data[1], y_pred)
+        self.val_ssims.append(ssim)
+        if self.verbose > 0:
+            print('Validation PSNR at epoch {epoch}: {self.val_psnrs[-1]}')
+            print('Validation SSIM at epoch {epoch}: {self.val_ssims[-1]}')
