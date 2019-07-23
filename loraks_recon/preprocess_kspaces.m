0ac192e2b0bd905e297900556a17b55410d13219
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-23 17:07:37 +0200

    made preprocessing closer to a step by step process, getting rid of indexing bugs

--- loraks_recon/preprocess_kspaces.m
+++ loraks_recon/preprocess_kspaces.m
@@ -1,16 +1,25 @@
 function [kspaces_out] = preprocess_kspaces(kspaces_in)
 
 kspaces = complex(kspaces_in.r, kspaces_in.i);
-kspaces = permute(kspaces, [2 1 3]);
+kspaces = permute(kspaces, [3 2 1]);
 kData = kspaces .*  sqrt(numel(kspaces(1, :, :)));
-% remove zero-padding and oversampled A/D
-kData = fftshift(ifft(ifftshift(kData,1),[],1),1);
-kData = fftshift(fft(ifftshift(kData(end/2+1+[-end/4:end/4-1],:,:))));
-% identify and remove zero-padding (works before applying the mask)
-[ii, jj, kk] = find(~ real(kData));
-[count, values] = hist(kk, unique(kk));
 k_size = size(kData);
+k_down_sampled = zeros(k_size(1), k_size(2) / 2, k_size(3));
+% remove zero-padding and oversampled A/D
+for i = 1:k_size(1)
+    k_slice = shiftdim(kData(i, :, :), 1);
+    k_slice = fftshift(ifft(ifftshift(k_slice,1),[],1),1);
+    k_slice = fftshift(fft(ifftshift(k_slice(end/2+1+[-end/4:end/4-1],:,:))));
+    k_down_sampled(i, :, :) = k_slice;
+end
+% identify and remove zero-padding (works before applying the mask)
+[ii, jj] = find(~ real(shiftdim(k_down_sampled(1, :, :), 1)));
+[count, values] = hist(jj, unique(jj));
+k_size = size(k_down_sampled);
 zero_values = values(count(:) == k_size(2));
-kData = kData(:,:,~zero_values);
+indexes = ones([k_size(3) 1]);
+indexes(zero_values) = 0;
+non_zero_values = find(indexes);
+kspaces_out = k_down_sampled(:,:,non_zero_values);
 end
 
