8ae4af62ba24e134d64d50755e1295eab565726a
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-29 16:37:01 +0200

    added pdnet to project

---
+++ pdnet.py
@@ -0,0 +1,68 @@
+from keras.layers import Input, Lambda, Multiply, Conv2D, concatenate, Add
+from keras.models import Model
+from keras.optimizers import Adam
+import tensorflow as tf
+from tensorflow.spectral import fft2d, ifft2d
+
+from utils import keras_psnr, keras_ssim
+
+
+def to_complex(x):
+    return tf.complex(x[0], x[1])
+
+def conv2d_complex(x, n_filters):
+    x_real = Lambda(tf.math.real)(x)
+    x_imag = Lambda(tf.math.imag)(x)
+    conv_real = Conv2D(
+        n_filters,
+        3,
+        activation='relu',
+        padding='same',
+        kernel_initializer='he_normal',
+    )(x_real)
+    conv_imag = Conv2D(
+        n_filters,
+        3,
+        activation='relu',
+        padding='same',
+        kernel_initializer='he_normal',
+    )(x_imag)
+    conv_res = Lambda(to_complex)([conv_real, conv_imag])
+    return conv_res
+
+def pdnet(n_filters=32, lr=1e-3, n_primal=5, n_dual=5, n_iter=10):
+    kspace_input = Input((640, None, 1), dtype='complex64')
+    mask = Input((640, None), dtype='complex64')
+
+
+    primal = Lambda(lambda x: tf.concat([tf.zeros_like(x)] * n_primal, axis=-1))(kspace_input)
+    dual = Lambda(lambda x: tf.concat([tf.zeros_like(kspace_input)] * n_dual, axis=-1))(kspace_input)
+
+    for i in range(n_iter):
+        # first work in kspace (dual space)
+        primal_sq = Lambda(tf.squeeze, output_shape=(640, None), arguments={'axis': -1})(Lambda(lambda x: x[..., 1:2])(primal))
+        dual_eval_fft = Lambda(fft2d, output_shape=(640, None))(primal_sq)
+        dual_eval_masked = Multiply()([dual_eval_fft, mask])
+        dual_eval_exp = Lambda(tf.expand_dims, output_shape=(640, None, 1), arguments={'axis': -1})(dual_eval_masked)
+        update = concatenate([dual, dual_eval_exp, kspace_input], axis=-1)
+
+        update = conv2d_complex(update, n_filters)
+        update = conv2d_complex(update, n_dual)
+        dual = Add()([dual, update])
+
+
+        # Then work in image space (primal space)
+        dual_sq = Lambda(tf.squeeze, output_shape=(640, None), arguments={'axis': -1})(Lambda(lambda x: x[..., 0:1])(dual))
+        dual_masked = Multiply()([dual_sq, mask])
+        primal_eval = Lambda(ifft2d, output_shape=(640, None))(dual_masked)
+        primal_exp = Lambda(tf.expand_dims, output_shape=(640, None, 1), arguments={'axis': -1})(primal_eval)
+        update = concatenate([primal, primal_exp], axis=-1)
+
+        update = conv2d_complex(update, n_filters)
+        update = conv2d_complex(update, n_primal)
+        primal = Add()([primal, update])
+
+    image_res = Lambda(lambda x: x[..., 0:1])(primal)
+
+    model = Model(inputs=[kspace_input, mask], outputs=image_res)
+    model.compile(optimizer=Adam(lr=lr), loss='mean_absolute_error', metrics=['mean_squared_error', keras_psnr, keras_ssim])
