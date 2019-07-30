71bf97917c1ff5475ac2d91104dad2d9ee47f342
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-30 11:44:24 +0200

    few corrections in LORKAS exp script

--- loraks_recon/val_metrics.m
+++ loraks_recon/val_metrics.m
@@ -2,19 +2,20 @@ f = waitbar(0,'Please wait...');
 val_path = '/media/Zaccharie/UHRes/singlecoil_val/';
 filelist = dir(strcat(val_path, '*.h5'));
 [n_files, un] = size(filelist);
-AF = 4;
-ssims = zeros(n_files);
-psnrs = zeros(n_files);
-for i = 1:n_files
-    waitbar(i/ n_files, f, i)
+AF = 8;
+ssims = zeros([n_files, 1]);
+psnrs = zeros([n_files, 1]);
+% for i = 1:n_files
+parfor i = 1:n_files
     filename = filelist(i).name;
     kspaces = h5read(strcat(val_path, filename), '/kspace');
     [p, s] = reco(kspaces, AF);
     psnrs(i) = p;
     ssims(i) = s;
+    waitbar(i/ n_files, f, i)
 end
-disp('Mean of PSNR', mean(psnrs))
-disp('Std dev of PSNR', stddev(psnrs))
-disp('Mean of SSIM', mean(ssims))
-disp('Std dev of SSIM', stddev(ssims))
+disp(strcat('Mean of PSNR :', num2str(mean(psnrs))))
+disp(strcat('Std dev of PSNR :', num2str(std(psnrs))))
+disp(strcat('Mean of SSIM :', num2str(mean(ssims))))
+disp(strcat('Std dev of SSIM :', num2str(std(ssims))))
 close(f)
