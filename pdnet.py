61aa7edd02d3c18125c72b38e0cc626cc8be3bdc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-07-31 11:18:59 +0200

    removed bias in conv layers (for now) and added extra non linearity

--- pdnet.py
+++ pdnet.py
@@ -25,6 +25,7 @@ def conv2d_complex(x, n_filters, activation='relu', output_shape=None):
         activation=act_real,
         padding='same',
         kernel_initializer='he_normal',
+        use_bias=False,
     )(x_real)
     conv_imag = Conv2D(
         n_filters,
@@ -32,6 +33,7 @@ def conv2d_complex(x, n_filters, activation='relu', output_shape=None):
         activation=act_comp,
         padding='same',
         kernel_initializer='he_normal',
+        use_bias=False,
     )(x_imag)
     conv_res = Lambda(to_complex, output_shape=output_shape)([conv_real, conv_imag])
     return conv_res
@@ -59,6 +61,12 @@ def pdnet(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_dual=5
     # inputs and buffers
     kspace_input = Input(input_size, dtype='complex64')
     mask = Input(mask_shape, dtype='complex64')
+
+    # # simple inverse
+    # image_res = Lambda(tf_ifft, output_shape=input_size, name='ifft')(kspace_input)
+    # image_res = conv2d_complex(image_res, n_filters, activation='relu', output_shape=conv_shape)
+    # image_res = conv2d_complex(image_res, 1, activation='linear', output_shape=input_size)
+
     primal = Lambda(lambda x: tf.concat([tf.zeros_like(x, dtype='complex64')] * n_primal, axis=-1), output_shape=primal_shape)(kspace_input)
     dual = Lambda(lambda x: tf.concat([tf.zeros_like(kspace_input, dtype='complex64')] * n_dual, axis=-1), output_shape=dual_shape)(kspace_input)
 
@@ -70,6 +78,7 @@ def pdnet(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_dual=5
         dual_eval_exp = Lambda(tf.expand_dims, output_shape=input_size, arguments={'axis': -1})(dual_eval_masked)
         update = concatenate([dual, dual_eval_exp, kspace_input], axis=-1)
 
+        update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape)
         update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape)
         update = conv2d_complex(update, n_dual, activation='linear', output_shape=dual_shape)
         dual = Add()([dual, update])
@@ -86,12 +95,13 @@ def pdnet(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_dual=5
         update = concatenate([primal, primal_exp], axis=-1)
 
         update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape)
+        update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape)
         update = conv2d_complex(update, n_dual, activation='linear', output_shape=primal_shape)
         primal = Add()([primal, update])
 
     image_res = Lambda(lambda x: x[..., 0:1], output_shape=input_size)(primal)
-    image_res = Lambda(tf.math.abs)(image_res)
 
+    image_res = Lambda(tf.math.abs)(image_res)
     model = Model(inputs=[kspace_input, mask], outputs=image_res)
     model.compile(optimizer=Adam(lr=lr), loss='mean_absolute_error', metrics=['mean_squared_error', keras_psnr, keras_ssim])
 
