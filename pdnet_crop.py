fa64c4df22a27322ae7378edfbdc487cdc3cb49b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-07 17:55:20 +0200

    added possibility of residual connection and made the handling of complex conv more like kikinet

--- pdnet_crop.py
+++ pdnet_crop.py
@@ -11,6 +11,14 @@ from utils import keras_psnr, keras_ssim
 def to_complex(x):
     return tf.complex(x[0], x[1])
 
+def concatenate_real_imag(x):
+    x_real = Lambda(tf.math.real)(x)
+    x_imag = Lambda(tf.math.imag)(x)
+    return concatenate([x_real, x_imag])
+
+def complex_from_half(x, n, output_shape):
+    return Lambda(lambda x: to_complex([x[..., :n], x[..., n:]]), output_shape=output_shape)(x)
+
 def conv2d_complex(x, n_filters, activation='relu', output_shape=None, idx=0):
     x_real = Lambda(tf.math.real, name=f'real_part_{idx}')(x)
     x_imag = Lambda(tf.math.imag, name=f'imag_part_{idx}')(x)
@@ -92,7 +100,7 @@ def invnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, **dummy_kwargs
 
     return model
 
-def pdnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_dual=5, n_iter=10):
+def pdnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_dual=5, n_iter=10, res_connection=False):
     # shapes
     mask_shape = input_size[:-1]
     primal_shape = list(input_size)
@@ -117,11 +125,38 @@ def pdnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_d
         # first work in kspace (dual space)
         dual_eval_exp = Lambda(tf_op, output_shape=input_size, arguments={'idx': 1}, name='fft_masked_{i}'.format(i=i+1))([primal, mask])
         update = concatenate([dual, dual_eval_exp, kspace_input], axis=-1)
+        update = concatenate_real_imag(update)
 
-        update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape, idx=f'{i}_1_primal')
-        update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape, idx=f'{i}_2_primal')
-        update = conv2d_complex(update, n_dual, activation='linear', output_shape=dual_shape, idx=f'{i}_linear_primal')
-        dual = Add()([dual, update])
+        update = Conv2D(
+            n_filters,
+            3,
+            activation='relu',
+            padding='same',
+            kernel_initializer='he_normal',
+            use_bias=False,
+        )(update)
+        update = Conv2D(
+            n_filters,
+            3,
+            activation='relu',
+            padding='same',
+            kernel_initializer='he_normal',
+            use_bias=False,
+        )(update)
+        update = Conv2D(
+            2 * n_dual,
+            3,
+            activation='linear',
+            padding='same',
+            kernel_initializer='he_normal',
+            use_bias=False,
+        )(update)
+        update = complex_from_half(update, n_dual, dual_shape)
+        if res_connection:
+            to_add = [dual, update, dual_eval_exp]
+        else:
+            to_add = [dual, update]
+        dual = Add()(to_add)
 
         # if only primal:
         # dual = dual_eval_exp - kspace_input
@@ -130,11 +165,38 @@ def pdnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_d
         # Then work in image space (primal space)
         primal_exp = Lambda(tf_adj_op, output_shape=input_size, name='ifft_masked_{i}'.format(i=i+1))([dual, mask])
         update = concatenate([primal, primal_exp], axis=-1)
+        update = concatenate_real_imag(update)
 
-        update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape, idx=f'{i}_1_dual')
-        update = conv2d_complex(update, n_filters, activation='relu', output_shape=conv_shape, idx=f'{i}_2_dual')
-        update = conv2d_complex(update, n_dual, activation='linear', output_shape=primal_shape, idx=f'{i}_linear_dual')
-        primal = Add()([primal, update])
+        update = Conv2D(
+            n_filters,
+            3,
+            activation='relu',
+            padding='same',
+            kernel_initializer='he_normal',
+            use_bias=False,
+        )(update)
+        update = Conv2D(
+            n_filters,
+            3,
+            activation='relu',
+            padding='same',
+            kernel_initializer='he_normal',
+            use_bias=False,
+        )(update)
+        update = Conv2D(
+            2 * n_primal,
+            3,
+            activation='linear',
+            padding='same',
+            kernel_initializer='he_normal',
+            use_bias=False,
+        )(update)
+        update = complex_from_half(update, n_primal, primal_shape)
+        if res_connection:
+            to_add = [primal, update, primal_exp]
+        else:
+            to_add = [primal, update]
+        primal = Add()(to_add)
 
     image_res = Lambda(lambda x: x[..., 0:1], output_shape=input_size, name='image_getting')(primal)
 
