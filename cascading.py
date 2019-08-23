70db9b29a768d8399bc0b8f6f34d7badc50f3448
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-23 15:04:47 +0200

    made the data consistency weight be learnable

--- cascading.py
+++ cascading.py
@@ -1,4 +1,3 @@
-from keras.initializers import Constant
 from keras.layers import Input, Lambda, Conv2D, Add, Layer
 from keras.models import Model
 from keras.optimizers import Adam
@@ -7,47 +6,37 @@ import tensorflow as tf
 from pdnet_crop import tf_adj_op, tf_op, concatenate_real_imag, complex_from_half, tf_crop
 from utils import keras_psnr, keras_ssim
 
-class DataConsistency(Layer):
-    def __init__(self, mask, kspace_input, sample_weight_init=1.0, **kwargs):
-        self.mask = mask
-        self.kspace_input = kspace_input
-        self.sample_weight_init = sample_weight_init
-        super(DataConsistency, self).__init__(**kwargs)
+class MultiplyScalar(Layer):
+    def __init__(self, **kwargs):
+        super(MultiplyScalar, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        assert isinstance(input_shape, list)
         # Create a trainable weight variable for this layer.
         self.sample_weight = self.add_weight(
             name='sample_weight',
             shape=(1,),
-            initializer=Constant(self.sample_weight_init),
+            initializer='ones',
             trainable=True,
         )
-        super(DataConsistency, self).build(input_shape)  # Be sure to call this at the end
+        super(MultiplyScalar, self).build(input_shape)  # Be sure to call this at the end
 
-    def call(self, image):
-        cnn_fft = tf_op([image, self.mask])
-        data_consistency_fourier = self.kspace_input - cnn_fft
-        data_consistency_image = tf_adj_op([data_consistency_fourier, self.mask])
-        data_consistency_image = self.sample_weight_init * data_consistency_image
-        image = image + data_consistency_image
-        return image
+    def call(self, x):
+        return tf.cast(self.sample_weight, tf.complex64) * x
 
     def compute_output_shape(self, input_shape):
         return input_shape
 
 
-def cascade_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, sw_init=1.0, lr=1e-3):
+def cascade_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, lr=1e-3):
     mask_shape = input_size[:-1]
     kspace_input = Input(input_size, dtype='complex64', name='kspace_input')
     mask = Input(mask_shape, dtype='complex64', name='mask_input')
-    lamb = 1.0
-    m = lamb / (1 + lamb)
+
     zero_filled = Lambda(tf_adj_op, output_shape=input_size, name='ifft_simple')([kspace_input, mask])
 
 
     image = zero_filled
-    dc_layer = DataConsistency(mask=mask, kspace_input=kspace_input, sample_weight_init=sw_init)
+    multiply_scalar = MultiplyScalar()
     for i in range(n_cascade):
         # residual convolution
         res_image = concatenate_real_imag(image)
@@ -71,8 +60,12 @@ def cascade_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16,
         res_image = complex_from_half(res_image, 1, input_size)
         image = Add(name='res_connex_{i}'.format(i=i+1))([image, res_image])
         # data consistency layer
-        image = dc_layer(image)
-
+        cnn_fft = Lambda(tf_op, output_shape=input_size, name='fft_masked_{i}'.format(i=i+1))([image, mask])
+        cnn_fft = Lambda(lambda x: -x, output_shape=input_size)(cnn_fft)
+        data_consistency_fourier = Add(name='data_consist_fft_{i}'.format(i=i+1))([kspace_input, cnn_fft])
+        data_consistency_image = Lambda(tf_adj_op, output_shape=input_size, name='ifft_masked_{i}'.format(i=i+1))([data_consistency_fourier, mask])
+        data_consistency_image = multiply_scalar(data_consistency_image)
+        image = Add(name='data_consist_enf_{i}'.format(i=i+1))([image, data_consistency_image])
 
     # module and crop of image
     image = Lambda(tf.math.abs, name='image_module', output_shape=input_size)(image)
