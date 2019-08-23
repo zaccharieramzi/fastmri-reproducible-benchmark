043448b71681cf3033f8319a208e0d74decd91f1
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-23 16:10:53 +0200

    added noiseless possibility when doing cascading

--- cascading.py
+++ cascading.py
@@ -3,7 +3,7 @@ from keras.models import Model
 from keras.optimizers import Adam
 import tensorflow as tf
 
-from pdnet_crop import tf_adj_op, tf_op, concatenate_real_imag, complex_from_half, tf_crop
+from pdnet_crop import tf_adj_op, tf_op, concatenate_real_imag, complex_from_half, tf_crop, tf_unmasked_adj_op, tf_unmasked_op
 from utils import keras_psnr, keras_ssim
 
 class MultiplyScalar(Layer):
@@ -26,8 +26,15 @@ class MultiplyScalar(Layer):
     def compute_output_shape(self, input_shape):
         return input_shape
 
+def replace_values_on_mask(x):
+    cnn_fft, kspace_input, mask = x
+    anti_mask = tf.expand_dims(tf.dtypes.cast(1.0 - mask, cnn_fft.dtype), axis=-1)
+    replace_cnn_fft = tf.math.multiply(anti_mask, cnn_fft) + kspace_input
+    return replace_cnn_fft
 
-def cascade_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, lr=1e-3):
+
+
+def cascade_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, noiseless=True, lr=1e-3):
     mask_shape = input_size[:-1]
     kspace_input = Input(input_size, dtype='complex64', name='kspace_input')
     mask = Input(mask_shape, dtype='complex64', name='mask_input')
@@ -60,12 +67,17 @@ def cascade_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16,
         res_image = complex_from_half(res_image, 1, input_size)
         image = Add(name='res_connex_{i}'.format(i=i+1))([image, res_image])
         # data consistency layer
-        cnn_fft = Lambda(tf_op, output_shape=input_size, name='fft_masked_{i}'.format(i=i+1))([image, mask])
-        cnn_fft = Lambda(lambda x: -x, output_shape=input_size)(cnn_fft)
-        data_consistency_fourier = Add(name='data_consist_fft_{i}'.format(i=i+1))([kspace_input, cnn_fft])
-        data_consistency_image = Lambda(tf_adj_op, output_shape=input_size, name='ifft_masked_{i}'.format(i=i+1))([data_consistency_fourier, mask])
-        data_consistency_image = multiply_scalar(data_consistency_image)
-        image = Add(name='data_consist_enf_{i}'.format(i=i+1))([image, data_consistency_image])
+        if noiseless:
+            cnn_fft = Lambda(tf_unmasked_op, output_shape=input_size, name='fft_simple_{i}'.format(i=i+1))(image)
+            data_consistency_fourier = Lambda(replace_values_on_mask, output_shape=input_size, name='fft_repl_{i}'.format(i=i+1))([cnn_fft, kspace_input, mask])
+            image = Lambda(tf_unmasked_adj_op, output_shape=input_size, name='ifft_simple_{i}'.format(i=i+1))(data_consistency_fourier)
+        else:
+            cnn_fft = Lambda(tf_op, output_shape=input_size, name='fft_masked_{i}'.format(i=i+1))([image, mask])
+            cnn_fft = Lambda(lambda x: -x, output_shape=input_size)(cnn_fft)
+            data_consistency_fourier = Add(name='data_consist_fft_{i}'.format(i=i+1))([kspace_input, cnn_fft])
+            data_consistency_image = Lambda(tf_adj_op, output_shape=input_size, name='ifft_masked_{i}'.format(i=i+1))([data_consistency_fourier, mask])
+            data_consistency_image = multiply_scalar(data_consistency_image)
+            image = Add(name='data_consist_enf_{i}'.format(i=i+1))([image, data_consistency_image])
 
     # module and crop of image
     image = Lambda(tf.math.abs, name='image_module', output_shape=input_size)(image)
