875b351132ce75040d442c1e23c0837cf2b5b10c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-26 11:20:10 +0200

    changed layer names to have name unicity

--- kiki.py
+++ kiki.py
@@ -45,7 +45,7 @@ def kiki_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, no
             use_bias=False,
         )(res_image)
         res_image = complex_from_half(res_image, 1, input_size)
-        image = Add(name='res_connex_{i}'.format(i=i+1))([image, res_image])
+        image = Add(name='i_res_connex_{i}'.format(i=i+1))([image, res_image])
         # data consistency layer
         cnn_fft = Lambda(tf_unmasked_op, output_shape=input_size, name='fft_simple_{i}'.format(i=i+1))(image)
         if noiseless:
@@ -76,7 +76,7 @@ def kiki_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, no
             use_bias=False,
         )(res_k_data)
         res_k_data = complex_from_half(res_k_data, 1, input_size)
-        data_consistency_fourier = Add(name='res_connex_{i}'.format(i=i+1))([data_consistency_fourier, res_k_data])
+        data_consistency_fourier = Add(name='k_res_connex_{i}'.format(i=i+1))([data_consistency_fourier, res_k_data])
 
         image = Lambda(tf_unmasked_adj_op, output_shape=input_size, name='ifft_simple_{i}'.format(i=i+1))(data_consistency_fourier)
 
