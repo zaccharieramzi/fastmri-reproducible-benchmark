d030e3e45b2361164fdcb88ccdf9b2279839c5b0
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-29 13:39:14 +0200

    added reconstruction from net accepting kspace and mask inputs

--- reconstruction.py
+++ reconstruction.py
@@ -91,3 +91,14 @@ def reco_and_gt_unet_from_val_file_no_norm(zero_img_batch, img_batch, model):
     im_recos = model.predict_on_batch(zero_img_batch)
     im_recos = np.squeeze(im_recos)
     return im_recos, np.squeeze(img_batch)
+
+
+def reco_net_from_test_file(kspace_and_mask_batch, model):
+    im_recos = model.predict_on_batch(kspace_and_mask_batch)
+    im_recos = np.squeeze(im_recos)
+    return im_recos
+
+def reco_and_gt_net_from_val_file(kspace_and_mask_batch, img_batch, model):
+    im_recos = reco_net_from_test_file(kspace_and_mask_batch, model)
+    img_batch = np.squeeze(img_batch)
+    return im_recos, img_batch
