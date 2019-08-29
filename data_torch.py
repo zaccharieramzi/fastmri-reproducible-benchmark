0643e465c24fa3338a7f11295532a9df660bf301
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-29 16:49:14 +0200

    implemented torch datasets

---
+++ data_torch.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from data import MaskedUntouched2DSequence
+
+class MaskedUntouched2DDataset(MaskedUntouched2DSequence, Dataset):
+
+    def get_item_train(self, filename):
+        ([kspaces, mask_batch], images) = super(MaskedUntouched2DDataset, self).get_item_train(filename)
+        images = torch.from_numpy(images[..., 0])
+        kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
+        return kspaces, mask_batch, images
+
+    def get_item_test(self, filename):
+        kspaces, mask_batch = super(MaskedUntouched2DDataset, self).get_item_test(filename)
+        kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
+        return kspaces, mask_batch
