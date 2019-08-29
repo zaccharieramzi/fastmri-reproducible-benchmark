a36b59b02ef48bc5684d8ede8d03dd5687c72875
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-29 18:02:00 +0200

    added pytorch version of kikinet

--- kiki.py
+++ kiki.py
@@ -2,10 +2,13 @@ from keras.layers import Input, Lambda, Conv2D, Add, Layer
 from keras.models import Model
 from keras.optimizers import Adam
 import tensorflow as tf
+import torch
+from torch import nn
 
-from cascading import MultiplyScalar, replace_values_on_mask
+from cascading import MultiplyScalar, replace_values_on_mask, ConvBlock, replace_values_on_mask_torch
 from pdnet_crop import tf_adj_op, tf_op, concatenate_real_imag, complex_from_half, tf_crop, tf_unmasked_adj_op, tf_unmasked_op
 from utils import keras_psnr, keras_ssim
+from transforms import ifft2, fft2, center_crop, complex_abs
 
 
 def mask_tf(x):
@@ -91,3 +94,39 @@ def kiki_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, no
     )
 
     return model
+
+
+class KikiNet(torch.nn.Module):
+    def __init__(self, n_cascade=5, n_convs=5, n_filters=16):
+        super(KikiNet, self).__init__()
+
+        self.n_cascade = n_cascade
+        self.n_convs = n_convs
+        self.n_filters = n_filters
+
+        self.i_conv_layers = nn.ModuleList([ConvBlock(n_convs, n_filters) for _ in range(n_cascade)])
+        self.k_conv_layers = nn.ModuleList([ConvBlock(n_convs, n_filters) for _ in range(n_cascade)])
+
+    def forward(self, kspace, mask):
+        zero_filled = ifft2(kspace)
+        image = zero_filled
+        # this because pytorch doesn't support NHWC
+        for i, i_conv_layer, k_conv_layer in enumerate(zip(self.i_conv_layers, self.k_conv_layers)):
+            # residual convolution
+            res_image = image
+            res_image = res_image.permute(0, 3, 1, 2)
+            res_image = i_conv_layer(res_image)
+            res_image = res_image.permute(0, 2, 3, 1)
+            image = image + res_image
+            # data consistency layer
+            cnn_fft = fft2(image)
+            data_consistency_fourier = replace_values_on_mask_torch(cnn_fft, kspace, mask)
+            data_consistency_fourier = data_consistency_fourier.permute(0, 3, 1, 2)
+            data_consistency_fourier = k_conv_layer(data_consistency_fourier)
+            data_consistency_fourier = data_consistency_fourier.permute(0, 2, 3, 1)
+            image = ifft2(data_consistency_fourier)
+
+        # equivalent of taking the module of the image
+        image = complex_abs(image)
+        image = center_crop(image, (320, 320))
+        return image
