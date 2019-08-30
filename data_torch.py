97396dea42001c27465d85c834419240e5ec6763
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-08-30 16:14:40 +0200

    corrected some mistakes in data torch, tensorizing the mask and a typo

--- data_torch.py
+++ data_torch.py
@@ -9,12 +9,14 @@ class MaskedUntouched2DDataset(MaskedUntouched2DSequence, Dataset):
     def get_item_train(self, filename):
         ([kspaces, mask_batch], images) = super(MaskedUntouched2DDataset, self).get_item_train(filename)
         images = torch.from_numpy(images[..., 0])
+        mask_batch = torch.from_numpy(mask_batch)
         kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
         return kspaces, mask_batch, images
 
     def get_item_test(self, filename):
         kspaces, mask_batch = super(MaskedUntouched2DDataset, self).get_item_test(filename)
         kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
+        mask_batch = torch.from_numpy(mask_batch)
         return kspaces, mask_batch
 
 
@@ -23,10 +25,12 @@ class MaskedUntouched2DAllLoadedDataset(MaskedUntouched2DAllLoadedSequence, Data
     def get_item_train(self, filename):
         ([kspaces, mask_batch], images) = super(MaskedUntouched2DAllLoadedDataset, self).get_item_train(filename)
         images = torch.from_numpy(images[..., 0])
+        mask_batch = torch.from_numpy(mask_batch)
         kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
         return kspaces, mask_batch, images
 
     def get_item_test(self, filename):
-        kspaces, mask_batch = super(MaskedUntouched2DDataset, self).get_item_test(filename)
+        kspaces, mask_batch = super(MaskedUntouched2DAllLoadedDataset, self).get_item_test(filename)
         kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
+        mask_batch = torch.from_numpy(mask_batch)
         return kspaces, mask_batch
