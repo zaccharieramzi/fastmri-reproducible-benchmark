ed26d7b2edc0331fd890984b1726b2e2c62d9824
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-04 16:32:06 +0200

    removed all the use_bias=False

--- kiki.py
+++ kiki.py
@@ -38,7 +38,6 @@ def kiki_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, no
                 activation='relu',
                 padding='same',
                 kernel_initializer='glorot_uniform',
-                use_bias=False,
             )(res_image)
         res_image = Conv2D(
             2,
@@ -46,7 +45,6 @@ def kiki_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, no
             activation='linear',
             padding='same',
             kernel_initializer='glorot_uniform',
-            use_bias=False,
         )(res_image)
         res_image = complex_from_half(res_image, 1, input_size)
         image = Add(name='res_connex_{i}'.format(i=i+1))([image, res_image])
@@ -69,7 +67,6 @@ def kiki_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, no
                 activation='relu',
                 padding='same',
                 kernel_initializer='glorot_uniform',
-                use_bias=False,
             )(data_consistency_fourier)
         data_consistency_fourier = Conv2D(
             2,
@@ -77,7 +74,6 @@ def kiki_net(input_size=(640, None, 1), n_cascade=5, n_convs=5, n_filters=16, no
             activation='linear',
             padding='same',
             kernel_initializer='glorot_uniform',
-            use_bias=False,
         )(data_consistency_fourier)
         data_consistency_fourier = complex_from_half(data_consistency_fourier, 1, input_size)
 
