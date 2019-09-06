16eadb51706292106faf774be9da3e8655ff9373
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-06 11:55:14 +0200

    added vd sampling to utils to test it out

--- utils.py
+++ utils.py
@@ -33,6 +33,29 @@ def gen_mask(kspace, accel_factor=8):
     mask = mask.reshape(*mask_shape)
     return mask
 
+def gen_mask_vd(kspace, accel_factor=8):
+    shape = kspace.shape
+    num_cols = shape[-1]
+
+    center_fraction = (32 // accel_factor) / 100
+    acceleration = accel_factor
+
+    # Create the mask
+    num_low_freqs = int(round(num_cols * center_fraction))
+    prob = (num_cols / acceleration - num_low_freqs) / (num_cols - 3 * num_low_freqs)
+    num_sampling = int(prob * num_cols)
+    selected_indexes = np.random.randint(0, num_cols, size=(num_sampling, 2)).mean(axis=-1).astype('int')
+    mask = np.zeros((num_cols,)).astype('bool')
+    mask[selected_indexes] = True
+    pad = (num_cols - num_low_freqs + 1) // 2
+    mask[pad:pad + num_low_freqs] = True
+
+    # Reshape the mask
+    mask_shape = [1 for _ in shape]
+    mask_shape[-1] = num_cols
+    mask = mask.reshape(*mask_shape)
+    return mask
+
 # taken from https://github.com/facebookresearch/fastMRI/
 def normalize(data, mean, stddev, eps=0.):
     """
