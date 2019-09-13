1616609074cfff8b7895a856089821da079438f9
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-13 16:16:31 +0200

    corrected chkpt path for torch scripts

--- cascadenet_torch_approach_af4.py
+++ cascadenet_torch_approach_af4.py
@@ -47,7 +47,7 @@ run_params = {
 
 n_epochs = 300
 run_id = f'cascadenet_torch_af{AF}_{int(time.time())}'
-chkpt_path = f'checkpoints/{run_id}' + '-{epoch:02d}.hdf5'
+chkpt_path = 'checkpoints'
 print(run_id)
 
 
