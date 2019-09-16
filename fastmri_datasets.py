55943303e1b4002139cbf1592bf9ce1e88da0ea1
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-16 19:00:58 +0200

    changed datasets name for greater consistency with sequences

--- fastmri_datasets.py
+++ fastmri_datasets.py
@@ -4,20 +4,20 @@ from torch.utils.data import Dataset
 
 from fastmri_sequences import Masked2DSequence
 
-class MaskedUntouched2DDataset(Masked2DSequence, Dataset):
+class Masked2DDataset(Masked2DSequence, Dataset):
     """Sequence equivalent for torch. Read Masked2DSequence docs for more.
     This dataset concatenates real and imaginary parts on a 5th axis because
     complex numbers are not handled in pytorch.
     """
     def get_item_train(self, filename):
-        ([kspaces, mask_batch], images) = super(MaskedUntouched2DDataset, self).get_item_train(filename)
+        ([kspaces, mask_batch], images) = super(Masked2DDataset, self).get_item_train(filename)
         images = torch.from_numpy(images[..., 0])
         mask_batch = torch.from_numpy(mask_batch)
         kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
         return kspaces, mask_batch, images
 
     def get_item_test(self, filename):
-        kspaces, mask_batch = super(MaskedUntouched2DDataset, self).get_item_test(filename)
+        kspaces, mask_batch = super(Masked2DDataset, self).get_item_test(filename)
         kspaces = torch.from_numpy(np.concatenate([kspaces.real, kspaces.imag], axis=-1))
         mask_batch = torch.from_numpy(mask_batch)
         return kspaces, mask_batch
