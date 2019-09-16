7bc2df280cf1e616cdf7bf2d38ee6b020e25f72c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-16 16:43:34 +0200

    added docs to the data utils

--- data_utils.py
+++ data_utils.py
@@ -1,9 +1,12 @@
+"""Utilities to get the data from the h5 files"""
 from functools import lru_cache
 
 import h5py
 
 
 def from_test_file_to_mask_and_kspace(filename):
+    """Get the mask and kspaces from an h5 file with 'mask' and 'kspace' keys.
+    """
     with  h5py.File(filename) as h5_obj:
         masks = h5_obj['mask'][()]
         kspaces = h5_obj['kspace'][()]
@@ -12,6 +15,9 @@ def from_test_file_to_mask_and_kspace(filename):
 
 @lru_cache(maxsize=128)
 def from_train_file_to_image_and_kspace(filename):
+    """Get the imagess and kspaces from an h5 file with 'reconstruction_esc'
+    and 'kspace' keys.
+    """
     with h5py.File(filename) as h5_obj:
         images = h5_obj['reconstruction_esc'][()]
         kspaces = h5_obj['kspace'][()]
@@ -19,6 +25,8 @@ def from_train_file_to_image_and_kspace(filename):
 
 
 def from_file_to_kspace(filename):
+    """Get the kspaces from an h5 file with 'kspace' keys.
+    """
     with h5py.File(filename) as h5_obj:
         kspaces = h5_obj['kspace'][()]
         return kspaces
