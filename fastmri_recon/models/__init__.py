23758ff67d77d4e04b29260cfba081dc433cd28e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-17 18:26:25 +0200

    add kiki net ref

--- fastmri_recon/models/__init__.py
+++ fastmri_recon/models/__init__.py
@@ -11,6 +11,10 @@ References
     Model Learning: Primal Dual Networks for Fast MR imaging
     https://arxiv.org/abs/1908.02426
 
+..[E2017] Taejoon Eo et al. (2017)
+    KIKI-net: cross-domain convolutional neural networks for reconstructing undersampled magnetic resonance images
+    https://www.ncbi.nlm.nih.gov/pubmed/29624729
+
 ..[S2017] Jo Schlemper et al. (2017)
     A Deep Cascade of Convolutional Neural Networks for MR Image Reconstruction
     https://arxiv.org/abs/1704.02422
