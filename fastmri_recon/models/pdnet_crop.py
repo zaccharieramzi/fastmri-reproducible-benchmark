52479483d915b72977ea13a050c2b077e12729df
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-17 17:21:01 +0200

    added docs for pdnet

--- fastmri_recon/models/pdnet_crop.py
+++ fastmri_recon/models/pdnet_crop.py
@@ -1,3 +1,4 @@
+"""Learned Primal-dual network adapted to MRI."""
 from keras.layers import Input, Lambda, concatenate, Add
 from keras.models import Model
 import tensorflow as tf
@@ -11,6 +12,36 @@ from ..helpers.transforms import ifft2, fft2, center_crop, complex_abs
 
 
 def pdnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_dual=5, n_iter=10, primal_only=False):
+    r"""This net unrolls the PDHG algorithm in the context of MRI
+
+    The original network is described in [A2017]. Its implementation is
+    available at https://github.com/adler-j/learned_primal_dual. A network
+    adapted to MRI has been re-implemented in [C2019] without known
+    public implementation. Here the Radon transform operator is replaced by the
+    Fourier Transform operator. Because of the needs of this particular
+    dataset (the size of the kspaces is not constant) we can't stick to an odl
+    implementation for the operator. We therefore stick to the TensorFlow
+    defined FFT. It also has the significant advantage to be able to leverage
+    CuFFT.
+    Because the kspace and image are complex numbers we simply need to adapt
+    the convolutions. The real and imaginary part of either are concatenated
+    along the last dimension, making a 2-channel image that we can then flow
+    through the convolutions. We then 're-complexify' the last output of the
+    convolutions.
+
+    Parameters:
+    input_size (tuple): the size of your input kspace
+    n_filters (int): number of filters in the convolution blocks
+    lr (float): learning rate
+    n_primal (int): number of elements in the primal memory buffer
+    n_dual (int): number of elements in the dual memory buffer
+    n_iter (int): number of PDHG unrolled iterations
+    primal_only (bool): whether to use non linearity in the dual space, or just
+        use the residual
+
+    Returns:
+    keras.models.Model: the primal dual net model, compiled
+    """
     # shapes
     mask_shape = input_size[:-1]
     primal_shape = list(input_size)
@@ -23,12 +54,11 @@ def pdnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_d
     # inputs and buffers
     kspace_input = Input(input_size, dtype='complex64', name='kspace_input')
     mask = Input(mask_shape, dtype='complex64', name='mask_input')
-
-
     primal = Lambda(lambda x: tf.concat([tf.zeros_like(x, dtype='complex64')] * n_primal, axis=-1), output_shape=primal_shape, name='buffer_primal')(kspace_input)
     if not primal_only:
         dual = Lambda(lambda x: tf.concat([tf.zeros_like(x, dtype='complex64')] * n_dual, axis=-1), output_shape=dual_shape, name='buffer_dual')(kspace_input)
 
+    # unrolled iterations
     for i in range(n_iter):
         # first work in kspace (dual space)
         dual_eval_exp = Lambda(tf_op, output_shape=input_size, arguments={'idx': 1}, name='fft_masked_{i}'.format(i=i+1))([primal, mask])
@@ -46,8 +76,8 @@ def pdnet_crop(input_size=(640, None, 1), n_filters=32, lr=1e-3, n_primal=5, n_d
         update = conv2d_complex(update, n_filters, 2, output_shape=primal_shape, res=False)
         primal = Add()([primal, update])
 
+    # formatting the image and finalizing the model definition
     image_res = Lambda(lambda x: x[..., 0:1], output_shape=input_size, name='image_getting')(primal)
-
     image_res = tf_fastmri_format(image_res)
     model = Model(inputs=[kspace_input, mask], outputs=image_res)
     default_model_compile(model, lr)
