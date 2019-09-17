95ae18865c0b096b92b72ee6d8a61bee507c8e43
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-17 14:44:33 +0200

    added scale factor for the kikinet training script

--- fastmri_recon/training_scripts/kikinet_approach_af4.py
+++ fastmri_recon/training_scripts/kikinet_approach_af4.py
@@ -43,8 +43,8 @@ n_volumes_val = 199
 
 # generators
 AF = 4
-train_gen = Masked2DSequence(train_path, af=AF, inner_slices=8, rand=True)
-val_gen = Masked2DSequence(val_path, af=AF)
+train_gen = Masked2DSequence(train_path, af=AF, inner_slices=8, rand=True, scale_factor=1e6)
+val_gen = Masked2DSequence(val_path, af=AF, scale_factor=1e6)
 
 
 
