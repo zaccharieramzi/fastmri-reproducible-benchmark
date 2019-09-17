b14fb792a4a1c6288dcf08fbee5c9d3a14e67d31
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-17 16:56:36 +0200

    renamed invnet crop zerofill net and put it in a separate file

---
+++ fastmri_recon/models/zerofill_net.py
@@ -0,0 +1,21 @@
+from keras.layers import Input, Lambda
+from keras.models import Model
+
+from ..helpers.keras_utils import default_model_compile
+from ..helpers.nn_mri_helpers import tf_fastmri_format, tf_adj_op
+
+
+def zerofill_net(input_size=(640, None, 1), **dummy_kwargs):
+    # shapes
+    mask_shape = input_size[:-1]
+    # inputs and buffers
+    kspace_input = Input(input_size, dtype='complex64', name='kspace_input_simple')
+    mask = Input(mask_shape, dtype='complex64', name='mask_input_simple')
+    # # simple inverse
+    image_res = Lambda(tf_adj_op, output_shape=input_size, name='ifft_simple')([kspace_input, mask])
+    image_res = tf_fastmri_format(image_res)
+    model = Model(inputs=[kspace_input, mask], outputs=image_res)
+    default_model_compile(model, lr=1e-3)
+
+
+    return model
