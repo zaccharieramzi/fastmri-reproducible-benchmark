987edf1c4de5581cbe37947a5ef0a87b6754fabb
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-09-28 18:14:59 +0200

    shortened number of epochs

--- fastmri_recon/training_scripts/kikinet_sep_approach_af4.py
+++ fastmri_recon/training_scripts/kikinet_sep_approach_af4.py
@@ -37,11 +37,11 @@ run_params = {
     'n_convs': 25,
     'n_filters': 48,
     'noiseless': True,
-    'lr': 1e-4,
+    'lr': 1e-3,
     'activation': lrelu,
 }
 multiply_scalar = MultiplyScalar()
-n_epochs = 100
+n_epochs = 20
 
 def train_model(model, space='K', n=1):
     print(model.summary(line_length=150))
