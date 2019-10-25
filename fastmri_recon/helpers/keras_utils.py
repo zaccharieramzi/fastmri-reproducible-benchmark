684edd345d551a757889781efa846dc6955e060a
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-25 10:48:00 +0200

    better discriminator monitoring, added mean output and accuracy

--- fastmri_recon/helpers/keras_utils.py
+++ fastmri_recon/helpers/keras_utils.py
@@ -13,3 +13,11 @@ def default_model_compile(model, lr):
 
 def wasserstein_loss(y_true, y_pred):
     return K.mean(y_true*y_pred)
+
+def mean_output(y_true, y_pred):
+    return K.mean(y_pred)
+
+def discriminator_accuracy(y_true, y_pred):
+    y_pred_binary = K.cast(K.less(y_pred, 0.5), 'float32')
+    y_true_binary = K.cast(K.greater(y_true, 0), 'float32')
+    return K.mean(K.cast(K.equal(y_true_binary, y_pred_binary), 'float32'))
