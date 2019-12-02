195b6001a3d84fce313cd67604852231fcba913e
Sophie Starck <sophie_starck@hotmail.fr>
2019-12-02 15:03:13 +0100

    added generator for data set

--- fastmri_recon/data/test_generators.py
+++ fastmri_recon/data/test_generators.py
@@ -44,6 +44,46 @@ class RandomShapeGenerator:
     @threadsafe_generator
     def flow_z_filled_random_shapes(self,):
         random_shapes_gen = self.flow_random_shapes()
-        for (kspaces, _), images in random_shapes_gen:
+        for (kspaces, mask_batch), images in random_shapes_gen:
             z_filled = zero_filled_recon(kspaces[..., 0], crop=False)[..., None]
-            yield z_filled, images
+            yield z_filled, images, mask_batch
+
+class CifarGenerator:
+    def __init__(self, af, size, data, batch_size=1):
+        self.af = af
+        self.data = data
+        self.size = size
+        self.batch_size = batch_size
+        self.im_shape = (batch_size, size, size, 1)
+        self.index = 0
+
+
+    @threadsafe_generator
+    def flow_images(self,):
+        while True:
+            images = np.empty(self.im_shape)
+            kspaces = np.empty(self.im_shape)
+            for i in range(self.batch_size):
+                image = self.data[self.index]
+                image = image.astype('float32')
+                image /= 255
+                kspace = fft(image)
+                images[i, ..., 0] = image
+                kspaces[i, ..., 0] = kspace
+                self.index += 1
+            mask = gen_mask(kspaces[0, ..., 0], accel_factor=self.af)
+            fourier_mask = np.repeat(mask.astype(np.float), self.size, axis=0)
+            mask_batch = np.repeat(fourier_mask[None, ...], len(kspaces), axis=0)[..., None]
+            kspaces *= mask_batch
+            mask_batch = mask_batch[..., 0]
+            yield (kspaces, mask_batch), images
+
+    @threadsafe_generator
+    def flow_z_filled_images(self,):
+        random_shapes_gen = self.flow_random_shapes()
+        for (kspaces, mask_batch), images in random_shapes_gen:
+            z_filled = zero_filled_recon(kspaces[..., 0], crop=False)[..., None]
+            yield z_filled, images, mask_batch
+
+    def reset(self,):
+        self.index = 0
