b22f4e0f0e0fccdbb95a54f8d1a6b5c39d2d1b5c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-12-05 19:14:48 +0100

    corrected url in setup.py (#24)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Tools to benchmark different reconstruction neural nets on the fastMRI dataset",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/fastmri-reproducible-benchmark",
+    url="https://github.com/zaccharieramzi/fastmri-reproducible-benchmark",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
