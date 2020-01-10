c55b4aadf1e04712e5d876547a745b5caa022030
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-01-10 14:46:16 +0100

    added function to get contrast from an h5 file (fastmri)

--- fastmri_recon/data/data_utils.py
+++ fastmri_recon/data/data_utils.py
@@ -27,3 +27,10 @@ def from_file_to_kspace(filename):
     with h5py.File(filename) as h5_obj:
         kspaces = h5_obj['kspace'][()]
         return kspaces
+
+def from_file_to_contrast(filename):
+    """Get the contrast from an h5 file.
+    """
+    with h5py.File(filename) as h5_obj:
+        contrast = h5_obj.attrs['acquisition']
+        return contrast
