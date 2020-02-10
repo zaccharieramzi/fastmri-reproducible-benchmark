c936e6959fd3eee73084828a182f56958c577b81
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-10 16:16:22 +0100

    removed legacy h5py import in evaluate module

--- fastmri_recon/helpers/evaluate.py
+++ fastmri_recon/helpers/evaluate.py
@@ -4,7 +4,6 @@ Copyright (c) Facebook, Inc. and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
-import h5py
 import numpy as np
 from runstats import Statistics
 from skimage.measure import compare_psnr, compare_ssim
