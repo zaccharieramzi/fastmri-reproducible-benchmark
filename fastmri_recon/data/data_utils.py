d0aa9e632166b7ef3a5d873766f6d41f30fbc7b2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-25 18:58:19 +0100

    added a way to get image kspace and contrast from h5 file

--- fastmri_recon/data/data_utils.py
+++ fastmri_recon/data/data_utils.py
@@ -2,6 +2,7 @@
 import h5py
 
 
+# TODO: refactor all of this !
 def from_test_file_to_mask_and_kspace(filename):
     """Get the mask and kspaces from an h5 file with 'mask' and 'kspace' keys.
     """
@@ -19,6 +20,16 @@ def from_train_file_to_image_and_kspace(filename):
         images = h5_obj['reconstruction_esc'][()]
         kspaces = h5_obj['kspace'][()]
     return images, kspaces
+
+def from_train_file_to_image_and_kspace_and_contrast(filename):
+    """Get the imagess and kspaces from an h5 file with 'reconstruction_esc'
+    and 'kspace' keys.
+    """
+    with h5py.File(filename, 'r') as h5_obj:
+        images = h5_obj['reconstruction_esc'][()]
+        kspaces = h5_obj['kspace'][()]
+        contrast = h5_obj.attrs['acquisition']
+    return images, kspaces, contrast
 
 
 def from_file_to_kspace(filename):
