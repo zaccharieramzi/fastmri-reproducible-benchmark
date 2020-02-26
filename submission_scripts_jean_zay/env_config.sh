e7709545dc13e830049c66c898323904ea979a64
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-26 17:12:23 +0100

    made the install of fastmri recon use scratch as tmp dir

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -3,7 +3,7 @@ module purge
 module load python/3.7.5
 module load tensorflow-gpu/py3/2.1.0
 
-pip install --no-cache-dir ./
+TMPDIR=$SCRATCH/tmp pip install --no-cache-dir ./
 
 export FASTMRI_DATA_DIR=$SCRATCH/
 export OASIS_DATA_DIR=$SCRATCH/
