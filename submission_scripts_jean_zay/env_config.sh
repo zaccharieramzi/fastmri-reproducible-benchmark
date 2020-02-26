50f93ea14b0bc63b2c097187597d1df4070298c1
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-26 17:09:55 +0100

    made the install of fastmri recon require no cache in submission script

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -3,7 +3,7 @@ module purge
 module load python/3.7.5
 module load tensorflow-gpu/py3/2.1.0
 
-pip install ./
+pip install --no-cache-dir ./
 
 export FASTMRI_DATA_DIR=$SCRATCH/
 export OASIS_DATA_DIR=$SCRATCH/
