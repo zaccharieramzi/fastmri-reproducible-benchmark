0510cec2fc9648cc663b341da9d0822052391eab
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-26 17:14:10 +0100

    export instead of just before in jz script

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -3,7 +3,8 @@ module purge
 module load python/3.7.5
 module load tensorflow-gpu/py3/2.1.0
 
-TMPDIR=$SCRATCH/tmp pip install --no-cache-dir ./
+export TMPDIR=$SCRATCH/tmp
+pip install --no-cache-dir ./
 
 export FASTMRI_DATA_DIR=$SCRATCH/
 export OASIS_DATA_DIR=$SCRATCH/
