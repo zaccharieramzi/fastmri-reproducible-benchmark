fdd240014e25d72c0b9f8f98aabd508497e1653c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-27 12:08:28 +0100

    removed loading of pytorch

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -2,7 +2,6 @@
 module purge
 module load python/3.7.5
 module load tensorflow-gpu/py3/2.1.0
-module load pytorch-gpu/py3/1.4.0  # legacy
 
 export TMPDIR=$SCRATCH/tmp
 pip install --no-cache-dir ./
