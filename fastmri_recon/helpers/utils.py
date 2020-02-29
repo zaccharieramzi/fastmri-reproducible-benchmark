corrected af computation in tf

fastmri_recon/helpers/utils.py
@@ -61,7 +61,7 @@ def gen_mask_tf(kspace, accel_factor):
 
 def tf_af(mask):
     mask_int = tf.dtypes.cast(mask, 'int32')
-    return tf.reduce_sum(mask_int) / tf.shape(mask_int)[0]
+    return tf.shape(mask_int)[0] / tf.reduce_sum(mask_int)
 
 def gen_mask_vd(kspace, accel_factor=8):
     shape = kspace.shape