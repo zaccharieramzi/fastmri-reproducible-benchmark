5082dc0d9742b9ee2091bdddbd79e41c3339d6a6
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-20 11:48:01 +0100

    got rid of troch mention in nn_mri

--- fastmri_recon/helpers/nn_mri.py
+++ fastmri_recon/helpers/nn_mri.py
@@ -1,4 +1,4 @@
-"""Module containing helpers for building NN for MRI reconstruction in pytorch and keras."""
+"""Module containing helpers for building NN for MRI reconstruction in tf."""
 import tensorflow as tf
 from tensorflow.keras.layers import Lambda, Conv2D, Layer, concatenate, Add, LeakyReLU
 from tensorflow.keras import regularizers
