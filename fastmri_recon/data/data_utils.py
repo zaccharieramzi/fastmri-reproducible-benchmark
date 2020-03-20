e362d44d3691d4c88de0c10229e8eb829e1681c4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-20 12:36:39 +0100

    refactored non test data utils and made stuff from h5 return singleton when list is of len 1

--- fastmri_recon/data/data_utils.py
+++ fastmri_recon/data/data_utils.py
@@ -16,6 +16,8 @@ def _from_file_to_stuff(filename, vals=None, attrs=None):
             stuff.append(h5_obj[val][()])
         for attr in attrs:
             stuff.append(h5_obj.attrs[attr])
+    if len(stuff) == 1:
+        stuff = stuff[0]
     return stuff
 
 def from_test_file_to_mask_and_kspace(filename):
@@ -31,20 +33,13 @@ def from_train_file_to_image_and_kspace(filename):
     """Get the imagess and kspaces from an h5 file with 'reconstruction_esc'
     and 'kspace' keys.
     """
-    with h5py.File(filename, 'r') as h5_obj:
-        images = h5_obj['reconstruction_esc'][()]
-        kspaces = h5_obj['kspace'][()]
-    return images, kspaces
+    return _from_file_to_stuff(filename, vals=['reconstruction_esc', 'kspace'])
 
 def from_train_file_to_image_and_kspace_and_contrast(filename):
     """Get the imagess and kspaces from an h5 file with 'reconstruction_esc'
     and 'kspace' keys.
     """
-    with h5py.File(filename, 'r') as h5_obj:
-        images = h5_obj['reconstruction_esc'][()]
-        kspaces = h5_obj['kspace'][()]
-        contrast = h5_obj.attrs['acquisition']
-    return images, kspaces, contrast
+    return _from_file_to_stuff(filename, vals=['reconstruction_esc', 'kspace'], attrs=['acquisition'])
 
 def from_test_file_to_mask_and_kspace_and_contrast(filename):
     """Get the imagess and kspaces from an h5 file with 'reconstruction_esc'
@@ -68,16 +63,12 @@ def from_test_file_to_mask_and_contrast(filename):
 def from_file_to_kspace(filename):
     """Get the kspaces from an h5 file with 'kspace' keys.
     """
-    with h5py.File(filename, 'r') as h5_obj:
-        kspaces = h5_obj['kspace'][()]
-        return kspaces
+    return _from_file_to_stuff(filename, vals=['kspace'])
 
 def from_file_to_contrast(filename):
     """Get the contrast from an h5 file.
     """
-    with h5py.File(filename, 'r') as h5_obj:
-        contrast = h5_obj.attrs['acquisition']
-        return contrast
+    return _from_file_to_stuff(filename, attrs=['acquisition'])
 
 def list_files_w_contrast_and_af(path, AF=4, contrast=None):
     filenames = glob.glob(path + '*.h5')
