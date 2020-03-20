33f2b197ab5fb43ff427c3b599cc7ac49a0f8f98
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-20 11:59:04 +0100

    removed legacy complex psnr and ssim

--- fastmri_recon/helpers/utils.py
+++ fastmri_recon/helpers/utils.py
@@ -131,14 +131,3 @@ def keras_ssim(y_true, y_pred):
     max_pixel = tf.math.reduce_max(y_true)
     min_pixel = tf.math.reduce_min(y_true)
     return tf.image.ssim(y_true, y_pred, max_pixel - min_pixel)
-
-
-def keras_psnr_complex(y_true, y_pred):
-    y_true_abs = tf.math.abs(y_true)
-    max_pixel = tf.math.reduce_max(y_true_abs)
-    return tf.image.psnr(y_true_abs, tf.math.abs(y_pred), max_pixel)
-
-def keras_ssim_complex(y_true, y_pred):
-    y_true_abs = tf.math.abs(y_true)
-    max_pixel = tf.math.reduce_max(y_true_abs)
-    return tf.image.ssim(y_true_abs, tf.math.abs(y_pred), max_pixel)
