8dee7404dd7cae5578f5a808ffdeb27c94da075c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-20 12:41:13 +0100

    refactored test data utils

--- fastmri_recon/data/data_utils.py
+++ fastmri_recon/data/data_utils.py
@@ -23,10 +23,7 @@ def _from_file_to_stuff(filename, vals=None, attrs=None):
 def from_test_file_to_mask_and_kspace(filename):
     """Get the mask and kspaces from an h5 file with 'mask' and 'kspace' keys.
     """
-    with  h5py.File(filename, 'r') as h5_obj:
-        masks = h5_obj['mask'][()]
-        kspaces = h5_obj['kspace'][()]
-    return masks, kspaces
+    return _from_file_to_stuff(filename, vals=['mask', 'kspace'])
 
 
 def from_train_file_to_image_and_kspace(filename):
@@ -45,20 +42,14 @@ def from_test_file_to_mask_and_kspace_and_contrast(filename):
     """Get the imagess and kspaces from an h5 file with 'reconstruction_esc'
     and 'kspace' keys.
     """
-    with h5py.File(filename, 'r') as h5_obj:
-        masks = h5_obj['mask'][()]
-        kspaces = h5_obj['kspace'][()]
-        contrast = h5_obj.attrs['acquisition']
-    return masks, kspaces, contrast
+    return _from_file_to_stuff(filename, vals=['mask', 'kspace'], attrs=['acquisition'])
 
 def from_test_file_to_mask_and_contrast(filename):
     """Get the imagess and kspaces from an h5 file with 'reconstruction_esc'
     and 'kspace' keys.
     """
-    with h5py.File(filename, 'r') as h5_obj:
-        masks = h5_obj['mask'][()]
-        contrast = h5_obj.attrs['acquisition']
-    return masks, contrast
+    return _from_file_to_stuff(filename, vals=['mask'], attrs=['acquisition'])
+
 
 def from_file_to_kspace(filename):
     """Get the kspaces from an h5 file with 'kspace' keys.
