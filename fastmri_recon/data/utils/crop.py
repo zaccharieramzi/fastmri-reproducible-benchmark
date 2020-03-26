da6f095519ee5cadc2235fbb6b4c50b976cf5403
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-26 16:56:57 +0100

    made crop work with 4d data

--- fastmri_recon/data/utils/crop.py
+++ fastmri_recon/data/utils/crop.py
@@ -2,7 +2,7 @@ def crop_center(img, cropx, cropy=None):
     # taken from https://stackoverflow.com/questions/39382412/crop-center-portion-of-a-numpy-image/39382475
     if cropy is None:
         cropy = cropx
-    y, x = img.shape
+    y, x = img.shape[-2:]
     startx = x//2 - (cropx//2)
     starty = y//2 - (cropy//2)
-    return img[starty:starty+cropy, startx:startx+cropx]
+    return img[..., starty:starty+cropy, startx:startx+cropx]
