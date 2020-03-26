d64cbced50ec037d4710404aef35c3381a598983
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-26 19:40:15 +0100

    created the first parallel muticoil network

---
+++ fastmri_recon/models/subclassed_models/parallel_multicoil.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+
+from ..utils.fastmri_format import _tf_crop
+
+
+class ParallelMulticoil(Model):
+    def __init__(self, submodel, **kwargs):
+        super(ParallelMulticoil, self).__init__(**kwargs)
+        self.submodel = submodel
+
+    def call(self, inputs):
+        reconstructed_slices = tf.map_fn(
+            self.call_single_slice,
+            inputs,
+            parallel_iterations=2,
+            back_prop=False,
+        )
+        cropped_reconstruction = _tf_crop(reconstructed_slices)
+        return cropped_reconstruction
+
+
+    def call_single_slice(self, inputs):
+        reconstructed_coil_images = self.submodel(inputs)
+        # rss reconstruction
+        rss_reconstruction = tf.norm(reconstructed_coil_images, axis=0)
+        return rss_reconstruction
