ce3970d3f34279581ecf0588fb79d798ea05feda
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-26 14:36:20 +0100

    corrected unet recon to deal with read-only incomprehensible error again

--- fastmri_recon/evaluate/reconstruction/unet_reconstruction.py
+++ fastmri_recon/evaluate/reconstruction/unet_reconstruction.py
@@ -4,8 +4,8 @@ import numpy as np
 def reco_unet_from_test_file(zero_img_batch, means, stddevs, model):
     im_recos = model.predict_on_batch(zero_img_batch)
     im_recos = np.squeeze(im_recos)
-    im_recos *= np.array(stddevs)[:, None, None]
-    im_recos += np.array(means)[:, None, None]
+    im_recos = im_recos * np.array(stddevs)[:, None, None]
+    im_recos = im_recos + np.array(means)[:, None, None]
     return im_recos
 
 def reco_and_gt_unet_from_val_file(zero_img_batch, img_batch, means, stddevs, model):
