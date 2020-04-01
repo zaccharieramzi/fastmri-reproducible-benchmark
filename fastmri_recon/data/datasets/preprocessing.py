4c14519ff8bb1716eef77fe349df4ccfe796d0cc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-01 15:16:03 +0200

    made single coil fastmri dataset also use h5 slice selection

--- fastmri_recon/data/datasets/preprocessing.py
+++ fastmri_recon/data/datasets/preprocessing.py
@@ -1,27 +1,17 @@
 import tensorflow as tf
 
-from .slice_selection import selected_slices
 from ..utils.masking.gen_mask_tf import gen_mask_tf
 
 
-def generic_from_kspace_to_masked_kspace_and_mask(AF=4, inner_slices=None, rand=False, scale_factor=1):
+def generic_from_kspace_to_masked_kspace_and_mask(AF=4, scale_factor=1):
     def from_kspace_to_masked_kspace_and_mask(images, kspaces):
         mask = gen_mask_tf(kspaces, accel_factor=AF)
         kspaces_masked = mask * kspaces
-        if inner_slices is not None:
-            slices = selected_slices(kspaces_masked, inner_slices=inner_slices, rand=rand)
-            kspaces_sliced = kspaces_masked[slices[0]:slices[1]]
-            images_sliced = images[slices[0]:slices[1]]
-            mask_sliced = mask[slices[0]:slices[1]]
-        else:
-            kspaces_sliced = kspaces_masked
-            images_sliced = images
-            mask_sliced = mask
-        kspaces_scaled = kspaces_sliced * scale_factor
-        images_scaled = images_sliced * scale_factor
+        kspaces_scaled = kspaces_masked * scale_factor
+        images_scaled = images * scale_factor
         kspaces_channeled = kspaces_scaled[..., None]
         images_channeled = images_scaled[..., None]
-        return (kspaces_channeled, mask_sliced), images_channeled
+        return (kspaces_channeled, mask), images_channeled
     return from_kspace_to_masked_kspace_and_mask
 
 def generic_prepare_mask_and_kspace(scale_factor=1):
