6eedb7e0d0a0ed704e2c4aaf42aeb5c4c7c9f451
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-01 11:04:01 +0200

    made a test for slicing

--- fastmri_recon/tests/data/utils/h5_test.py
+++ fastmri_recon/tests/data/utils/h5_test.py
@@ -9,15 +9,19 @@ from fastmri_recon.data.utils.h5 import (
     from_file_to_contrast,
 )
 
-test_file = 'file1000002.h5'
+test_file_single_coil = 'file1000002.h5'
 
-@pytest.mark.skipif(not os.path.isfile(test_file), reason='test file not present for h5 utils.')
-def test_all_functions():
-    functions_to_test = [
+@pytest.mark.skipif(not os.path.isfile(test_file_single_coil), reason='test file not present for h5 utils.')
+def test_all_functions_single_coil():
+    functions_sliceable = [
         from_train_file_to_image_and_kspace,
         from_train_file_to_image_and_kspace_and_contrast,
         from_file_to_kspace,
-        from_file_to_contrast,
     ]
+
+    functions_to_test = functions_sliceable + [from_file_to_contrast]
     for fun in functions_to_test:
-        fun(test_file)
+        fun(test_file_single_coil)
+    for fun in functions_sliceable:
+        # this gets the 10-th slice of the volume
+        fun(test_file_single_coil, slices=(10))
