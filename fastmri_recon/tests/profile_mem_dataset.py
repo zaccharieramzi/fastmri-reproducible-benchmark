ec0c815123847a0d88e614d0538c387b6c625a7d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-07 17:50:16 +0200

    added dataset profiling script to tests

---
+++ fastmri_recon/tests/profile_mem_dataset.py
@@ -0,0 +1,22 @@
+from fastmri_recon.config import *
+from fastmri_recon.data.datasets.multicoil.fastmri_pyfunc import train_masked_kspace_dataset_from_indexable
+
+val_path = f'{FASTMRI_DATA_DIR}multicoil_val/'
+epochs = 5
+n_iter = 10
+
+val_set = train_masked_kspace_dataset_from_indexable(
+    val_path,
+    AF=4,
+    contrast=None,
+    inner_slices=8,
+    rand=True,
+    scale_factor=1e6,
+    parallel=False,
+    n_samples=n_iter,
+)
+
+for e in range(epochs):
+    training_iter = iter(val_set)
+    for i in range(n_iter):
+        res = next(training_iter)
