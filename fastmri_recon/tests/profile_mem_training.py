e81a54bc6465fc0ed581ad955831dce9f06b579d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-07 18:11:51 +0200

    changed mem training to use fit with an overly simple model, in order to reproduce best the real case

--- fastmri_recon/tests/profile_mem_training.py
+++ fastmri_recon/tests/profile_mem_training.py
@@ -1,9 +1,10 @@
 from fastmri_recon.config import *
 from fastmri_recon.data.datasets.multicoil.fastmri_pyfunc import train_masked_kspace_dataset_from_indexable
+from fastmri_recon.models.subclassed_models.pdnet import PDNet
 
 val_path = f'{FASTMRI_DATA_DIR}multicoil_val/'
-epochs = 50
-n_iter = 10
+epochs = 10
+n_iter = 30
 
 val_set = train_masked_kspace_dataset_from_indexable(
     val_path,
@@ -16,7 +17,15 @@ val_set = train_masked_kspace_dataset_from_indexable(
     n_samples=None,
 )
 
-for e in range(epochs):
-    training_iter = iter(val_set)
-    for i in range(n_iter):
-        res = next(training_iter)
+model = PDNet(n_filters=4, n_primal=1, n_dual=1, primal_only=True, n_iter=1, multicoil=True, activation='linear')
+model.compile(
+    optimizer='adam',
+    loss='mean_absolute_error',
+)
+
+model.fit(
+    val_set,
+    steps_per_epoch=n_iter,
+    epochs=epochs,
+    verbose=0,
+)
