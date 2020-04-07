b7d169aba387e63c2e014ad59c5f7324aa9fad90
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-07 18:08:34 +0200

    added more epochs in script for profiling

--- fastmri_recon/tests/profile_mem_training.py
+++ fastmri_recon/tests/profile_mem_training.py
@@ -2,7 +2,7 @@ from fastmri_recon.config import *
 from fastmri_recon.data.datasets.multicoil.fastmri_pyfunc import train_masked_kspace_dataset_from_indexable
 
 val_path = f'{FASTMRI_DATA_DIR}multicoil_val/'
-epochs = 5
+epochs = 50
 n_iter = 10
 
 val_set = train_masked_kspace_dataset_from_indexable(
