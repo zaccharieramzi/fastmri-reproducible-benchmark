c119798fb46d1dbcc23906e5f6dbdd5f97faea41
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-14 12:01:02 +0200

    corrected config for tf without module

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 module purge
-module load python/3.7.5
-module load tensorflow-gpu/py3/2.1.0
+module load python/3.7.5 cuda/10.1.1 cudnn/7.6.5.32-cuda-10.1
 
 export TMPDIR=$SCRATCH/tmp
 pip install --no-cache-dir ./
