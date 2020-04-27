5b8e168524cb536efb2bee2b9a2492f118685884
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-27 16:06:39 +0200

    added different shapes for the u pdnet test

--- fastmri_recon/tests/models/subclassed_models/updnet_test.py
+++ fastmri_recon/tests/models/subclassed_models/updnet_test.py
@@ -4,15 +4,16 @@ import tensorflow as tf
 from fastmri_recon.models.subclassed_models.updnet import UPDNet
 
 
-@pytest.mark.parametrize('model_kwargs', [
-    {},
-    {'primal_only': True},
+@pytest.mark.parametrize('model_kwargs, n_phase_encoding', [
+    ({}, 320),
+    ({}, 550),
+    ({'primal_only': True}, 320),
 ])
-def test_pdnet_init_and_call(model_kwargs):
+def test_pdnet_init_and_call(model_kwargs, n_phase_encoding):
     model = UPDNet(**model_kwargs)
     model([
-        tf.zeros([1, 640, 320, 1], dtype=tf.complex64),  # kspace
-        tf.zeros([1, 640, 320], dtype=tf.complex64),  # mask
+        tf.zeros([1, 640, n_phase_encoding, 1], dtype=tf.complex64),  # kspace
+        tf.zeros([1, 640, n_phase_encoding], dtype=tf.complex64),  # mask
     ])
 
 def test_pdnet_multicoil_init_and_call():
