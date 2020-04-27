cb7d4297a51bfea30d9cbe663e4b226251b76245
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-27 14:50:02 +0200

    added corresponding unit test for complex unet

---
+++ fastmri_recon/tests/models/subclassed_models/unet_test.py
@@ -0,0 +1,17 @@
+import pytest
+import tensorflow as tf
+
+from fastmri_recon.models.subclassed_models.unet import UnetComplex
+
+
+@pytest.mark.parametrize('model_kwargs', [
+    {},
+    {'n_output_channels': 6},
+    {'res': True},
+])
+def test_cnn_complex_init_call(model_kwargs):
+    model = UnetComplex(**model_kwargs)
+    model(tf.zeros(
+        [1, 640, 320, model_kwargs.get('n_output_channels', 1)],
+        dtype=tf.complex64,
+    ))
