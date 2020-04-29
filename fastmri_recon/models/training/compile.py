f989d098060b02153ebef27a3ed4c0a5c85fc14e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-04-29 16:43:44 +0200

    made norm clipping depend on use of mixed precision

--- fastmri_recon/models/training/compile.py
+++ fastmri_recon/models/training/compile.py
@@ -1,11 +1,16 @@
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
 from tensorflow.keras.optimizers import Adam
 
 from ...evaluate.metrics.tf_metrics import keras_psnr, keras_ssim
 
 
 def default_model_compile(model, lr):
+    opt_kwargs = {}
+    precision_policy = mixed_precision.global_policy()
+    if precision_policy.loss_scale is None:
+        opt_kwargs['clipnorm'] = 1.
     model.compile(
-        optimizer=Adam(lr=lr),
+        optimizer=Adam(lr=lr, **opt_kwargs),
         loss='mean_absolute_error',
         metrics=['mean_squared_error', keras_psnr, keras_ssim],
     )
