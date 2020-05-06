cb27b528086da70beb016fc60e7a0678afb27bf0
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-06 11:02:49 +0200

    added smap refinement in the updnet training script

--- fastmri_recon/training_scripts/multi_coil/updnet_approach_sense.py
+++ fastmri_recon/training_scripts/multi_coil/updnet_approach_sense.py
@@ -26,6 +26,7 @@ def train_updnet(
         base_n_filter=16,
         non_linearity='relu',
         channel_attention_kwargs=None,
+        refine_smaps=False,
         loss='mae',
         original_run_id=None,
     ):
@@ -75,6 +76,7 @@ def train_updnet(
         'non_linearity': non_linearity,
         'n_iter': n_iter,
         'channel_attention_kwargs': channel_attention_kwargs,
+        'refine_smaps': refine_smaps,
     }
     additional_info = f'af{af}'
     if contrast is not None:
@@ -93,6 +95,8 @@ def train_updnet(
         additional_info += f'_{loss}'
     if channel_attention_kwargs:
         additional_info += '_ca'
+    if refine_smaps:
+        additional_info += '_rf_sm'
 
     run_id = f'updnet_sense_{additional_info}_{int(time.time())}'
     chkpt_path = f'{CHECKPOINTS_DIR}checkpoints/{run_id}' + '-{epoch:02d}.hdf5'
