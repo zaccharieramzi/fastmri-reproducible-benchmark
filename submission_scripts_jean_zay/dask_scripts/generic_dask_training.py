3acceb665bff12fb7f98000e61c3868590634ffa
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-07 15:25:11 +0200

    added generic eval for dask

--- submission_scripts_jean_zay/dask_scripts/generic_dask_training.py
+++ submission_scripts_jean_zay/dask_scripts/generic_dask_training.py
@@ -36,3 +36,41 @@ def train_on_jz_dask(job_name, train_function, *args, **kwargs):
     )
     client.gather(futures)
     print('Shutting down dask workers')
+
+def eval_on_jz_dask(job_name, eval_function, *args, **kwargs):
+    cluster = SLURMCluster(
+        cores=1,
+        job_cpu=40,
+        memory='80GB',
+        job_name=job_name,
+        walltime='20:00:00',
+        interface='ib0',
+        job_extra=[
+            f'--gres=gpu:4',
+            '--qos=qos_gpu-t3',
+            '--distribution=block:block',
+            '--hint=nomultithread',
+            '--output=%x_%j.out',
+        ],
+        env_extra=[
+            'cd $WORK/fastmri-reproducible-benchmark',
+            '. ./submission_scripts_jean_zay/env_config.sh',
+        ],
+    )
+    cluster.scale(1)
+
+    print(cluster.job_script())
+
+    client = Client(cluster)
+    futures = client.submit(
+        # function to execute
+        eval_function,
+        *args,
+        **kwargs,
+        # this function has potential side effects
+        pure=True,
+    )
+    metrics_names, eval_res = client.gather(futures)
+    print(metrics_names)
+    print(eval_res)
+    print('Shutting down dask workers')
