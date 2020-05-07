a35e5523f982513116bc0c63cf117f648e066f6b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-07 18:13:37 +0200

    added test files to conftest fake dataset

--- fastmri_recon/tests/conftest.py
+++ fastmri_recon/tests/conftest.py
@@ -1,7 +1,6 @@
 import h5py
 import numpy as np
 import pytest
-from _pytest.monkeypatch import MonkeyPatch
 from tqdm import tqdm
 
 
@@ -10,23 +9,30 @@ K_shape_multi_coil = (2, 15, 640, 322)
 I_shape = (2, 320, 320)
 contrast = 'CORPD_FBK'
 
-def create_data(filename, multicoil=False):
+def create_data(filename, multicoil=False, train=True):
     k_shape = K_shape_single_coil
-    image_ds = "reconstruction_esc"
-    if multicoil:
-        k_shape = K_shape_multi_coil
-        image_ds = "reconstruction_rss"
+    if train:
+        image_ds = "reconstruction_esc"
+        if multicoil:
+            k_shape = K_shape_multi_coil
+            image_ds = "reconstruction_rss"
+        image = np.random.normal(size=I_shape)
+        image = image.astype(np.float32)
+    else:
+        mask_shape = [K_shape_multi_coil[-1]]
+        mask = np.random.choice(a=[True, False], size=mask_shape)
     kspace = np.random.normal(size=k_shape) + 1j * np.random.normal(size=k_shape)
-    image = np.random.normal(size=I_shape)
     kspace = kspace.astype(np.complex64)
-    image = image.astype(np.float32)
     with h5py.File(filename, "w") as h5_obj:
         h5_obj.create_dataset("kspace", data=kspace)
-        h5_obj.create_dataset(image_ds, data=image)
+        if train:
+            h5_obj.create_dataset(image_ds, data=image)
+        else:
+            h5_obj.create_dataset('mask', data=mask)
         h5_obj.attrs['acquisition'] = contrast
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session", autouse=False)
 def create_full_fastmri_test_tmp_dataset(tmpdir_factory):
     # main dirs
     fastmri_tmp_data_dir = tmpdir_factory.mktemp(
@@ -60,6 +66,13 @@ def create_full_fastmri_test_tmp_dataset(tmpdir_factory):
     for i in tqdm(range(n_files), 'Creating single coil val files'):
         data_filename = f"val_singlecoil_{i}.h5"
         create_data(str(fastmri_tmp_singlecoil_val.join(data_filename)))
+    for i in tqdm(range(n_files), 'Creating single coil test files'):
+        data_filename = f"test_singlecoil_{i}.h5"
+        create_data(
+            str(fastmri_tmp_multicoil_val.join(data_filename)),
+            multicoil=False,
+            train=False,
+        )
     #### multi coil
     fastmri_tmp_multicoil_train = tmpdir_factory.mktemp(str(
         fastmri_tmp_data_dir.join('multicoil_train')
@@ -81,6 +94,13 @@ def create_full_fastmri_test_tmp_dataset(tmpdir_factory):
         create_data(
             str(fastmri_tmp_multicoil_val.join(data_filename)),
             multicoil=True,
+        )
+    for i in tqdm(range(n_files), 'Creating multi coil test files'):
+        data_filename = f"test_multicoil_{i}.h5"
+        create_data(
+            str(fastmri_tmp_multicoil_val.join(data_filename)),
+            multicoil=True,
+            train=False,
         )
     return {
         'fastmri_tmp_data_dir': str(fastmri_tmp_data_dir) + '/',
