6a3b107ff9e50dae8a01b5ef98ccf2e7b606dbd7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-12 18:26:52 +0200

    corrected updnet test names and added a use case corresponding to the submission 4 (smaller in n iterations though)

--- fastmri_recon/tests/models/subclassed_models/updnet_test.py
+++ fastmri_recon/tests/models/subclassed_models/updnet_test.py
@@ -9,7 +9,7 @@ from fastmri_recon.models.subclassed_models.updnet import UPDNet
     ({}, 550),
     ({'primal_only': True}, 320),
 ])
-def test_pdnet_init_and_call(model_kwargs, n_phase_encoding):
+def test_updnet_init_and_call(model_kwargs, n_phase_encoding):
     model = UPDNet(**model_kwargs)
     model([
         tf.zeros([1, 640, n_phase_encoding, 1], dtype=tf.complex64),  # kspace
@@ -20,8 +20,14 @@ def test_pdnet_init_and_call(model_kwargs, n_phase_encoding):
     {},
     {'channel_attention_kwargs': {'dense': True}},
     {'refine_smaps': True},
+    {
+        'channel_attention_kwargs': {'dense': True},
+        'refine_smaps': True,
+        'loss': 'compound_mssim',
+        'non_linearity': 'prelu',
+    }
 ])
-def test_pdnet_multicoil_init_and_call(model_kwargs):
+def test_updnet_multicoil_init_and_call(model_kwargs):
     model = UPDNet(primal_only=True, multicoil=True, **model_kwargs)
     model([
         tf.zeros([1, 5, 640, 320, 1], dtype=tf.complex64),  # kspace
