7491a54ef6ad361c155cb6414ca4a2887a2153bc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-12 23:41:00 +0200

    added more tests for the attention block

--- fastmri_recon/tests/models/utils/attention_test.py
+++ fastmri_recon/tests/models/utils/attention_test.py
@@ -1,8 +1,16 @@
+import pytest
 import tensorflow as tf
 
 from fastmri_recon.models.utils.attention import ChannelAttentionBlock
 
 
+@pytest.mark.parametrize('kwargs',[
+    {},
+    {'activation': 'prelu'},
+    {'activation': 'lrelu'},
+    {'dense': True},
+    {'dense': True, 'activation': 'prelu'},
+])
 def test_channel_attention_block_call():
     ca_block = ChannelAttentionBlock()
     inputs = tf.random.normal([2, 32, 32, 16])
