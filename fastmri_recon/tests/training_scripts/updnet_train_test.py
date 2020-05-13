4601a11c4d6382a087e63915d9093ead1713dc3b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-13 14:18:48 +0200

    corrected test for updnet train using kwargs and added single coil testing

--- fastmri_recon/tests/training_scripts/updnet_train_test.py
+++ fastmri_recon/tests/training_scripts/updnet_train_test.py
@@ -3,17 +3,26 @@ import pytest
 from fastmri_recon.training_scripts import updnet_train
 from fastmri_recon.training_scripts.updnet_train import train_updnet
 
-@pytest.mark.parametrize('args',[
-    ('1', None, '0123', 2, 1, 2, True, 2, 2),
-    ('1', None, '0123', 2, 1, 2, False, 2, 2),
-    ('1', None, '0123', 2, 1, 2, False, 2, 2, 'relu', None, False, 'compound_mssim'),
-    ('1', None, '0123', 2, 1, 2, False, 2, 2, 'prelu', None, False, 'mae'),
-    ('1', None, '0123', 2, 1, 2, False, 2, 2, 'prelu', None, True),
+@pytest.mark.parametrize('kwargs',[
+    {'use_mixed_precision': True},
+    {'loss': 'compound_mssim'},
+    {'non_linearity': 'prelu'},
+    {'non_linearity': 'prelu', 'refine_smaps': True},
+    {'fixed_masks': True},
+    {'multicoil': False},
 ])
-def test_train_updnet(create_full_fastmri_test_tmp_dataset, args):
+def test_train_updnet(create_full_fastmri_test_tmp_dataset, kwargs):
     updnet_train.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
     updnet_train.LOGS_DIR = create_full_fastmri_test_tmp_dataset['logs_tmp_dir']
     updnet_train.CHECKPOINTS_DIR = create_full_fastmri_test_tmp_dataset['checkpoints_tmp_dir']
     updnet_train.n_volumes_train = 2
-    train_updnet(*args)
+    train_updnet(
+        af=1,
+        n_samples=2,
+        n_epochs=1,
+        n_iter=1,
+        n_layers=2,
+        base_n_filter=2,
+        **kwargs,
+    )
     # TODO: checks that the checkpoints and the logs are correctly created
