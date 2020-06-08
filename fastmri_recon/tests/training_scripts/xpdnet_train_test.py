d77988f7c6abd34c03d6d819376cf18920655953
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-08 19:16:29 +0200

    added all models to xpdnet train test

--- fastmri_recon/tests/training_scripts/xpdnet_train_test.py
+++ fastmri_recon/tests/training_scripts/xpdnet_train_test.py
@@ -14,6 +14,28 @@ n_primal = 2
     # dncnn
     (DnCNN(n_convs=2, n_filters=4, res=False, n_outputs=2*n_primal), 0, True),
     # focnet
+    (FocNet(n_filters=4, n_outputs=2*n_primal), 4, False),
+    # mwcnn
+    (MWCNN(
+        n_scales=3,
+        kernel_size=3,
+        bn=False,
+        n_filters_per_scale=[4, 8, 8],
+        n_convs_per_scale=[2, 2, 2],
+        n_first_convs=2,
+        first_conv_n_filters=4,
+        res=False,
+        n_outputs=2*n_primal,
+    ), 3, True),
+    # unet
+    (unet(
+        n_layers=2,
+        layers_n_channels=[16, 32,],
+        res=False,
+        layers_n_non_lins=1,
+        n_output_channels=2*n_primal,
+        input_size=(None, None, 2*(n_primal + 1)),
+    ), 2, True),
 ])
 def test_train_xpdnet(create_full_fastmri_test_tmp_dataset, model, n_scales, res):
     xpdnet_train.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
@@ -25,6 +47,7 @@ def test_train_xpdnet(create_full_fastmri_test_tmp_dataset, model, n_scales, res
         n_scales=n_scales,
         n_primal=n_primal,
         res=res,
+        multicoil=False,
         af=1,
         n_samples=2,
         n_epochs=1,
