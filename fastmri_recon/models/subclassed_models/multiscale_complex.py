35f4b60e869813e11062c40ebf344c6edad7be0b
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-08 19:10:04 +0200

    in case of no scales, refrain from padding in multiscale complex

--- fastmri_recon/models/subclassed_models/multiscale_complex.py
+++ fastmri_recon/models/subclassed_models/multiscale_complex.py
@@ -21,18 +21,20 @@ class MultiscaleComplex(Model):
 
     def call(self, inputs):
         outputs = inputs
-        n_pad = 2**self.n_scales - tf.math.floormod(tf.shape(inputs)[-2], 2**(self.n_scales-1))
-        paddings = [
-            (0, 0),
-            (0, 0),  # here in the context of fastMRI there is nothing to worry about because the dim is 640 (128 x 5)
-            (n_pad//2, n_pad//2),
-            (0, 0),
-        ]
-        outputs = tf.pad(outputs, paddings)
+        if self.n_scales > 0:
+            n_pad = 2**self.n_scales - tf.math.floormod(tf.shape(inputs)[-2], 2**(self.n_scales-1))
+            paddings = [
+                (0, 0),
+                (0, 0),  # here in the context of fastMRI there is nothing to worry about because the dim is 640 (128 x 5)
+                (n_pad//2, n_pad//2),
+                (0, 0),
+            ]
+            outputs = tf.pad(outputs, paddings)
         outputs = tf.concat([tf.math.real(outputs), tf.math.imag(outputs)], axis=-1)
         outputs = self.model(outputs)
         outputs = to_complex(outputs, self.n_output_channels)
-        outputs = outputs[:, :, n_pad//2:-n_pad//2]
+        if self.n_scales > 0:
+            outputs = outputs[:, :, n_pad//2:-n_pad//2]
         if self.res:
             outputs = inputs[..., :self.n_output_channels] + outputs
         return outputs
