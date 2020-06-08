366dc3873e62d65dd55b8408247aa1a04b24e424
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-08 18:31:07 +0200

    added training scripts for denoising

---
+++ fastmri_recon/training_scripts/denoising/generic_train.py
@@ -0,0 +1,63 @@
+import os.path as op
+
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from tensorflow_addons.callbacks import TQDMProgressBar
+
+from fastmri_recon.config import *
+from fastmri_recon.data.datasets.fastmri_pyfunc_denoising import train_noisy_dataset_from_indexable
+from fastmri_recon.models.training.compile import default_model_compile
+
+
+n_volumes_train = 973
+
+def train_denoiser(
+        model,
+        run_id,
+        noise_std=30,
+        contrast=None,
+        n_samples=None,
+        n_epochs=200,
+        loss='mae',
+        lr=1e-3,
+    ):
+    train_path = f'{FASTMRI_DATA_DIR}singlecoil_train/singlecoil_train/'
+    val_path = f'{FASTMRI_DATA_DIR}singlecoil_val/'
+    train_set = train_noisy_dataset_from_indexable(
+        train_path,
+        noise_std=noise_std,
+        contrast=contrast,
+        inner_slices=None,
+        rand=True,
+        scale_factor=1e6,
+        n_samples=n_samples,
+    )
+    val_set = train_noisy_dataset_from_indexable(
+        val_path,
+        noise_std=noise_std,
+        contrast=contrast,
+        inner_slices=None,
+        rand=True,
+        scale_factor=1e6,
+    )
+    chkpt_path = f'{CHECKPOINTS_DIR}checkpoints/{run_id}' + '-{epoch:02d}.hdf5'
+    chkpt_cback = ModelCheckpoint(chkpt_path, period=n_epochs, save_weights_only=True)
+    log_dir = op.join(f'{LOGS_DIR}logs', run_id)
+    tboard_cback = TensorBoard(
+        profile_batch=0,
+        log_dir=log_dir,
+        histogram_freq=0,
+        write_graph=False,
+        write_images=False,
+    )
+    tqdm_cback = TQDMProgressBar()
+    default_model_compile(model, lr=lr, loss=loss)
+    model.fit(
+        train_set,
+        steps_per_epoch=n_volumes_train,
+        epochs=n_epochs,
+        validation_data=val_set,
+        validation_steps=2,
+        verbose=0,
+        callbacks=[tboard_cback, chkpt_cback, tqdm_cback],
+    )
+    return run_id
---
+++ fastmri_recon/training_scripts/denoising/mwcnn_train.py
@@ -0,0 +1,12 @@
+import time
+
+from .generic_train import train_denoiser
+from fastmri_recon.models.subclassed_models.denoisers.mwcnn import MWCNN
+
+
+def train_focnet(name='big', train_kwargs=None, **model_kwargs):
+    run_id = f'mwcnn_denoising_{name}_{int(time.time())}'
+    model = MWCNN(**model_kwargs)
+    if train_kwargs is None:
+        train_kwargs = {}
+    return train_denoiser(model, run_id, **train_kwargs)
