e75b429de233e27f50af0133b113d5863b2fc727
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-11 11:03:10 +0200

    added a channel attention u-net to test its efficiency

--- fastmri_recon/models/subclassed_models/denoisers/proposed_params.py
+++ fastmri_recon/models/subclassed_models/denoisers/proposed_params.py
@@ -48,6 +48,13 @@ params_per_model['U-net']['medium'] = dict(
     layers_n_non_lins=2,
     res=False,
 )
+params_per_model['U-net']['medium-ca'] = dict(
+    n_layers=4,
+    layers_n_channels=[16, 32, 64, 128],
+    layers_n_non_lins=2,
+    res=False,
+    channel_attention_kwargs={'dense': True},
+)
 params_per_model['U-net']['small'] = dict(
     n_layers=3,
     layers_n_channels=[16, 32, 64],
