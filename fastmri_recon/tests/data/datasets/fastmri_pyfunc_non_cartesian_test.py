834a9feed7e364025a21bb650461f34d60b97350
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-17 12:10:39 +0200

    corrected nc dataset to return original shape and adapted unit test accordingly

--- fastmri_recon/tests/data/datasets/fastmri_pyfunc_non_cartesian_test.py
+++ fastmri_recon/tests/data/datasets/fastmri_pyfunc_non_cartesian_test.py
@@ -10,14 +10,19 @@ spokelength = 400
 image_size = [640, 474]
 file_contrast = 'CORPD_FBK'
 
-@pytest.mark.parametrize('ds_kwargs, expected_kspace_shape', [
-    ({}, [image_shape[0], 1, nspokes*spokelength]),
-    ({'inner_slices': 1}, [1, 1, nspokes*spokelength]),
-    ({'inner_slices': 1, 'rand': True}, [1, 1, nspokes*spokelength]),
-    ({'contrast': file_contrast}, [image_shape[0], 1, nspokes*spokelength]),
-    ({'n_samples': 1}, [image_shape[0], 1, nspokes*spokelength]),
+@pytest.mark.parametrize('ds_kwargs, expected_kspace_shape, orig_shape', [
+    ({}, [image_shape[0], 1, nspokes*spokelength], image_shape[-2]),
+    ({'inner_slices': 1}, [1, 1, nspokes*spokelength], image_shape[-2]),
+    ({'inner_slices': 1, 'rand': True}, [1, 1, nspokes*spokelength], image_shape[-2]),
+    ({'contrast': file_contrast}, [image_shape[0], 1, nspokes*spokelength], image_shape[-2]),
+    ({'n_samples': 1}, [image_shape[0], 1, nspokes*spokelength], image_shape[-2]),
 ])
-def test_train_nc_kspace_dataset_from_indexable(create_full_fastmri_test_tmp_dataset, ds_kwargs, expected_kspace_shape):
+def test_train_nc_kspace_dataset_from_indexable(
+        create_full_fastmri_test_tmp_dataset,
+        ds_kwargs,
+        expected_kspace_shape,
+        orig_shape,
+    ):
     tf.config.experimental_run_functions_eagerly(True)
     path = create_full_fastmri_test_tmp_dataset['fastmri_tmp_singlecoil_train']
     ds = train_nc_kspace_dataset_from_indexable(
@@ -27,8 +32,9 @@ def test_train_nc_kspace_dataset_from_indexable(create_full_fastmri_test_tmp_dat
         spokelength=spokelength,
         **ds_kwargs,
     )
-    (kspace, traj), image = next(iter(ds))
+    (kspace, traj, orig_shape), image = next(iter(ds))
     # shape verifications
     assert kspace.shape.as_list() == expected_kspace_shape
+    assert orig_shape.numpy() == orig_shape
     assert traj.shape.as_list() == [expected_kspace_shape[0], 2, nspokes * spokelength]
     assert image.shape.as_list() == expected_kspace_shape[0:1] + [320, 320, 1]
