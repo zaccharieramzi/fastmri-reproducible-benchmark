42bc1cba8c53213fdc924e83a95c8c9ae06f65d2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-19 17:16:21 +0200

    corrected grappa recon to work with 3D data

--- fastmri_recon/evaluate/reconstruction/grappa_reconstruction.py
+++ fastmri_recon/evaluate/reconstruction/grappa_reconstruction.py
@@ -5,14 +5,16 @@ from ...data.utils.crop import crop_center
 
 
 def reco_grappa(kspace, af=4):
-    sy = kspace.shape[-1]
+    n_slices, _, sx, sy = kspace.shape[:]
     if af == 4:
         n_acs = int(sy * 8 / 100)
     else:
         n_acs = int(sy * 4 / 100)
     ctr, pd = sy // 2, n_acs // 2
     calib = kspace[..., ctr-pd:ctr+pd].copy() # call copy()!
-    recon = cgrappa(kspace, calib, kernel_size=(5, 5), coil_axis=0)
+    recon = np.empty([n_slices, sx, sy])
+    for i in range(n_slices):
+        recon[i] = cgrappa(kspace[i], calib, kernel_size=(5, 5), coil_axis=0)
     x_final = np.abs(recon)
     x_final = crop_center(x_final, 320)
     return x_final
