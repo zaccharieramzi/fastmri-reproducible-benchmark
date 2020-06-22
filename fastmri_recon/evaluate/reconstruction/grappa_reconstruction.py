d5f49d36d3d4399821b116c2b2c4855fa329fa12
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-22 16:01:18 +0200

    corrected recon grappa shape

--- fastmri_recon/evaluate/reconstruction/grappa_reconstruction.py
+++ fastmri_recon/evaluate/reconstruction/grappa_reconstruction.py
@@ -5,14 +5,14 @@ from ...data.utils.crop import crop_center
 
 
 def reco_grappa(kspace, af=4):
-    n_slices, _, sx, sy = kspace.shape[:]
+    n_slices, _, _, sy = kspace.shape[:]
     if af == 4:
         n_acs = int(sy * 8 / 100)
     else:
         n_acs = int(sy * 4 / 100)
     ctr, pd = sy // 2, n_acs // 2
     calib = kspace[..., ctr-pd:ctr+pd].copy() # call copy()!
-    recon = np.empty([n_slices, sx, sy], dtype=np.complex)
+    recon = np.empty_like(kspace)
     for i in range(n_slices):
         recon[i] = cgrappa(kspace[i].astype(np.complex), calib[i].astype(np.complex), kernel_size=(5, 5), coil_axis=0)
     x_final = np.linalg.norm(recon, axis=1)
