62365098b032cfad98db54ca92aa55c5f9d166e4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-24 17:45:38 +0200

    added possibility to use density compensation in preprocessing

--- fastmri_recon/data/datasets/preprocessing.py
+++ fastmri_recon/data/datasets/preprocessing.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tfkbnufft import kbnufft_forward, kbnufft_adjoint
+from tfkbnufft.mri.dcomp_calc import calculate_radial_dcomp_tf
 
 from ..utils.masking.gen_mask_tf import gen_mask_tf
 from ..utils.non_cartesian import get_radial_trajectory, get_debugging_cartesian_trajectory
@@ -16,7 +18,7 @@ def generic_from_kspace_to_masked_kspace_and_mask(AF=4, scale_factor=1, fixed_ma
         return (kspaces_channeled, mask), images_channeled
     return from_kspace_to_masked_kspace_and_mask
 
-def non_cartesian_from_kspace_to_nc_kspace_and_traj(nfft_ob, image_size, acq_type='radial', scale_factor=1, **acq_kwargs):
+def non_cartesian_from_kspace_to_nc_kspace_and_traj(nfft_ob, image_size, acq_type='radial', scale_factor=1, compute_dcomp=False, **acq_kwargs):
     def from_kspace_to_nc_kspace_and_traj(images, kspaces):
         if acq_type == 'radial':
             traj = get_radial_trajectory(**acq_kwargs)
@@ -24,6 +26,15 @@ def non_cartesian_from_kspace_to_nc_kspace_and_traj(nfft_ob, image_size, acq_typ
             traj = get_debugging_cartesian_trajectory()
         else:
             raise NotImplementedError(f'{acq_type} dataset not implemented yet.')
+        if compute_dcomp:
+            nufftob_forw = kbnufft_forward(nfft_ob)
+            nufftob_back = kbnufft_adjoint(nfft_ob)
+            dcomp = calculate_radial_dcomp_tf(
+                nfft_ob,
+                nufftob_forw,
+                nufftob_back,
+                traj[0],
+            )
         traj = tf.repeat(traj, tf.shape(images)[0], axis=0)
         orig_image = tf_unmasked_adj_op(kspaces[..., None])
         nc_kspace = nufft(nfft_ob, orig_image[:, None, ..., 0], traj, image_size)
@@ -32,7 +43,10 @@ def non_cartesian_from_kspace_to_nc_kspace_and_traj(nfft_ob, image_size, acq_typ
         images_channeled = images_scaled[..., None]
         nc_kspaces_channeled = nc_kspace_scaled[..., None]
         orig_shape = tf.ones([tf.shape(kspaces)[0]], dtype=tf.int32) * tf.shape(kspaces)[-1]
-        return (nc_kspaces_channeled, traj, (orig_shape,)), images_channeled
+        extra_args = (orig_shape,)
+        if compute_dcomp:
+            extra_args += (dcomp,)
+        return (nc_kspaces_channeled, traj, extra_args), images_channeled
     return tf.function(
         from_kspace_to_nc_kspace_and_traj,
         input_signature=[
