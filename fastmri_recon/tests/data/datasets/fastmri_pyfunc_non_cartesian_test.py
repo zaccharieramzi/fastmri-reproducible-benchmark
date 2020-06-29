3a13c1ea4cf7531ae53b7eafe0941016a9f58770
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-29 12:20:09 +0200

    added spiral ds test and removed eager run from ds test

--- fastmri_recon/tests/data/datasets/fastmri_pyfunc_non_cartesian_test.py
+++ fastmri_recon/tests/data/datasets/fastmri_pyfunc_non_cartesian_test.py
@@ -27,7 +27,6 @@ def test_train_nc_kspace_dataset_from_indexable(
         orig_shape,
         use_af,
     ):
-    tf.config.experimental_run_functions_eagerly(True)
     path = create_full_fastmri_test_tmp_dataset['fastmri_tmp_singlecoil_train']
     ds = train_nc_kspace_dataset_from_indexable(
         path,
@@ -42,3 +41,17 @@ def test_train_nc_kspace_dataset_from_indexable(
     assert shape.numpy()[0] == orig_shape
     assert traj.shape.as_list() == [expected_kspace_shape[0], 2, 640 * (474//af)]
     assert image.shape.as_list() == expected_kspace_shape[0:1] + [320, 320, 1]
+
+def test_spiral_dataset(create_full_fastmri_test_tmp_dataset):
+    path = create_full_fastmri_test_tmp_dataset['fastmri_tmp_singlecoil_train']
+    ds = train_nc_kspace_dataset_from_indexable(
+        path,
+        image_size,
+        af=af
+    )
+    (kspace, traj, (shape,)), image = next(iter(ds))
+    # shape verifications
+    assert kspace.shape.as_list() == kspace_shape
+    assert shape.numpy()[0] == image_shape[-2]
+    assert traj.shape.as_list() == [kspace_shape[0], 2, 640 * (474//af)]
+    assert image.shape.as_list() == kspace_shape[0:1] + [320, 320, 1]
