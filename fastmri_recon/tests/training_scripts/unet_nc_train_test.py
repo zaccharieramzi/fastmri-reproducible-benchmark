ce9b5fa00b0cc416729297b21c8ac8674a85190a
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-02 10:41:05 +0200

    corrected package modifications in train test

--- fastmri_recon/tests/training_scripts/unet_nc_train_test.py
+++ fastmri_recon/tests/training_scripts/unet_nc_train_test.py
@@ -1,17 +1,19 @@
 import pytest
 
-from fastmri_recon.training_scripts import ncpdnet_train
+from fastmri_recon.training_scripts import unet_nc_train
 from fastmri_recon.training_scripts.unet_nc_train import train_unet
 
+import tensorflow as tf
+tf.config.experimental_run_functions_eagerly(True)
 
 @pytest.mark.parametrize('kwargs',[
     {'dcomp': True},
 ])
 def test_train_unet(create_full_fastmri_test_tmp_dataset, kwargs):
-    ncpdnet_train.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
-    ncpdnet_train.LOGS_DIR = create_full_fastmri_test_tmp_dataset['logs_tmp_dir']
-    ncpdnet_train.CHECKPOINTS_DIR = create_full_fastmri_test_tmp_dataset['checkpoints_tmp_dir']
-    ncpdnet_train.n_volumes_train = 2
+    unet_nc_train.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
+    unet_nc_train.LOGS_DIR = create_full_fastmri_test_tmp_dataset['logs_tmp_dir']
+    unet_nc_train.CHECKPOINTS_DIR = create_full_fastmri_test_tmp_dataset['checkpoints_tmp_dir']
+    unet_nc_train.n_volumes_train = 2
     train_unet(
         af=8,
         n_samples=2,
