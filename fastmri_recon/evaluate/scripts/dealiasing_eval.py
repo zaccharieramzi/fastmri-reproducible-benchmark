3369d8ab139ec2b48bcd85e6e9103ac983c7fc58
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-03 11:47:03 +0200

    made eval function in dealiasers to use batches of 1

--- fastmri_recon/evaluate/scripts/dealiasing_eval.py
+++ fastmri_recon/evaluate/scripts/dealiasing_eval.py
@@ -1,9 +1,10 @@
 import os
 
-import tensorflow as tf
+from tqdm import tqdm
 
 from fastmri_recon.config import *
 from fastmri_recon.data.datasets.fastmri_pyfunc import train_masked_kspace_dataset_from_indexable as singlecoil_dataset
+from fastmri_recon.evaluate.metrics.np_metrics import METRIC_FUNCS, Metrics
 from fastmri_recon.models.subclassed_models.denoisers.proposed_params import build_model_from_specs
 from fastmri_recon.models.subclassed_models.multiscale_complex import MultiscaleComplex
 
@@ -34,24 +35,9 @@ def evaluate_xpdnet_dealiasing(
     )
     if n_samples is not None:
         val_set = val_set.take(n_samples)
+    else:
+        val_set = val_set.take(199)
 
-
-    def tf_psnr(y_true, y_pred):
-        perm_psnr = [3, 1, 2, 0]
-        psnr = tf.image.psnr(
-            tf.transpose(y_true, perm_psnr),
-            tf.transpose(y_pred, perm_psnr),
-            tf.reduce_max(y_true),
-        )
-        return psnr
-    def tf_ssim(y_true, y_pred):
-        perm_ssim = [0, 1, 2, 3]
-        ssim = tf.image.ssim(
-            tf.transpose(y_true, perm_ssim),
-            tf.transpose(y_pred, perm_ssim),
-            tf.reduce_max(y_true),
-        )
-        return ssim
     sub_model = build_model_from_specs(model_fun, model_kwargs, 2)
     model = MultiscaleComplex(
         sub_model,
@@ -59,7 +45,9 @@ def evaluate_xpdnet_dealiasing(
         n_scales=n_scales,
         fastmri_format=True,
     )
-    model.compile(loss=tf_psnr, metrics=[tf_ssim])
     model.load_weights(f'{CHECKPOINTS_DIR}checkpoints/{run_id}-{n_epochs:02d}.hdf5')
-    eval_res = model.evaluate(val_set, verbose=1, steps=199 if n_samples is None else None)
-    return model.metrics_names, eval_res
+    m = Metrics(METRIC_FUNCS)
+    for x, y_true in tqdm(val_set.as_numpy_iterator(), total=199 if n_samples is None else n_samples):
+        y_pred = model.predict(x, batch_size=1)
+        m.push(y_true[..., 0], y_pred[..., 0])
+    return ['PSNR', 'SSIM'], m.means().values()
