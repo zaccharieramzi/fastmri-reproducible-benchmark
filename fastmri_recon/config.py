862ba5f65e3c462c57be18aa07bd74a701f28816
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-09 17:59:45 +0200

    indicated better the oasis data directory

--- fastmri_recon/config.py
+++ fastmri_recon/config.py
@@ -3,6 +3,6 @@
 import os
 
 FASTMRI_DATA_DIR = os.environ.get('FASTMRI_DATA_DIR', '/media/Zaccharie/UHRes/')
-OASIS_DATA_DIR = os.environ.get('OASIS_DATA_DIR', '/media/Zaccharie/UHRes/')
+OASIS_DATA_DIR = os.environ.get('OASIS_DATA_DIR', '/media/Zaccharie/UHRes/OASIS_data')
 LOGS_DIR = os.environ.get('LOGS_DIR', './')
 CHECKPOINTS_DIR = os.environ.get('CHECKPOINTS_DIR', './')
