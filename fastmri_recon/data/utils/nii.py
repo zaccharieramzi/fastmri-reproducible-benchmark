665527fa6802d44d8e40257a1cbefaefbee53081
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-09 18:44:00 +0200

    added reversing in oasis files loading

--- fastmri_recon/data/utils/nii.py
+++ fastmri_recon/data/utils/nii.py
@@ -2,9 +2,13 @@ import nibabel as nib
 import numpy as np
 
 
-def from_file_to_volume(filename):
+def from_file_to_volume(filename, reverse=True):
+    # the smallest dimension is the ear-to-ear direction it appears
+    # the other dimensions seem to always be in the following order:
+    # back-to-front then top-to-bottom
     volume = nib.load(filename)
     volume = volume.get_fdata()
-    volume = volume[..., None]
+    if reverse and min(volume.shape) != volume.shape[0]:
+        volume = np.moveaxis(volume, -1, 0)
     volume = volume.astype(np.complex64)
     return volume
