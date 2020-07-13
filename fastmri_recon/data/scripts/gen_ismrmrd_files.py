fc60235f0575688641baf50d2c5ae9fb27b9eb4a
zaccharieramzi <zaccharie.ramzi@gmail.com>
2020-07-13 06:58:19 -0700

    made the ismrmrd script adaptable to different setups (af, organ, split)

--- fastmri_recon/data/scripts/gen_ismrmrd_files.py
+++ fastmri_recon/data/scripts/gen_ismrmrd_files.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 
+import click
+
 from fastmri_recon.config import FASTMRI_DATA_DIR
 from fastmri_recon.data.utils.ismrmrd import from_fastmri_to_ismrmrd
 
 
-if __name__ == '__main__':
-    af = 4
-    out = f'./val_knee_{af}/'
-    fastmri_path = Path(FASTMRI_DATA_DIR) / 'multicoil_val'
+def generate_ismrmrd(af=4, split='val', organ='knee'):
+    original_directory = f'multicoil_{split}'
+    if organ == 'brain':
+        original_directory = 'brain_' + original_directory
+    out = f'./{split}_{organ}_{af}/'
+    fastmri_path = Path(FASTMRI_DATA_DIR) / original_directory
     filenames = fastmri_path.glob('*.h5')
     for f in filenames:
-        from_fastmri_to_ismrmrd(filename, out_dir='./', accel_factor=af)
+        from_fastmri_to_ismrmrd(filename, out_dir=out, accel_factor=af, split=split)
