df0705ce4d98cbe51bda1542e21609dae728a254
zaccharieramzi <zaccharie.ramzi@gmail.com>
2020-07-13 05:55:51 -0700

    added equidistant mask for numpy

--- fastmri_recon/data/utils/masking/gen_mask.py
+++ fastmri_recon/data/utils/masking/gen_mask.py
@@ -22,6 +22,32 @@ def gen_mask(kspace, accel_factor=8, seed=None):
     mask = mask.reshape(*mask_shape)
     return mask
 
+def gen_mask_equidistant(kspace, accel_factor=8, seed=None):
+    # inspired by https://github.com/facebookresearch/fastMRI/blob/master/common/subsample.py
+    shape = kspace.shape
+    num_cols = shape[-1]
+
+    center_fraction = (32 // accel_factor) / 100
+    acceleration = accel_factor
+
+    # Create the mask
+    num_low_freqs = int(round(num_cols * center_fraction))
+    num_high_freqs = num_cols // accel_factor - num_low_freqs
+    high_freqs_spacing = (num_cols - num_low_freqs) // num_high_freqs
+    acs_lim = (num_cols - num_low_freqs + 1) // 2
+    mask_offset = random.randrange(high_freqs_spacing)
+    high_freqs_location = np.arange(mask_offset, num_cols, high_freqs_spacing)
+    low_freqs_location = np.arange(acs_lim, acs_lim + num_low_freqs)
+    mask_locations = np.concatentae(high_freqs_location, low_freqs_location)
+    mask = np.zeros((num_cols,))
+    mask[mask_locations] = True
+
+    # Reshape the mask
+    mask_shape = [1 for _ in shape]
+    mask_shape[-1] = num_cols
+    mask = mask.reshape(*mask_shape)
+    return mask
+
 def gen_mask_vd(kspace, accel_factor=8):
     shape = kspace.shape
     num_cols = shape[-1]
