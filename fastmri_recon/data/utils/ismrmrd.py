0c864d141fc01fdd9bb2fbec20b64eb148518d6d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-15 16:43:06 +0200

    added a scaling factor to the ismrmrd data, added the accel factor in the file

--- fastmri_recon/data/utils/ismrmrd.py
+++ fastmri_recon/data/utils/ismrmrd.py
@@ -8,14 +8,15 @@ from .h5 import _from_file_to_stuff
 from .masking.gen_mask import gen_mask_equidistant
 
 
-def kspace_to_ismrmrd(kspace, header, mask, file_index, out_dir='./'):
+def kspace_to_ismrmrd(kspace, header, mask, file_index, out_dir='./', accel_factor=4, scale_factor=1e6):
     header = ismrmrd.xsd.CreateFromDocument(header)
     header.encoding[0].encodingLimits.kspace_encoding_step_1.maximum = kspace.shape[-1]
     header.encoding[0].encodingLimits.kspace_encoding_step_1.center = kspace.shape[-1] // 2
+    header.encoding[0].parallelImaging.accelerationFactor.kspace_encoding_step_1 = accel_factor
     header = header.toxml()
     n_slices = kspace.shape[0]
     for i_slice in range(n_slices):
-        kspace_slice = kspace[i_slice]
+        kspace_slice = kspace[i_slice] * scale_factor
         path = Path(out_dir) / f'{file_index}_slice_{i_slice}.h5'
         ds = ismrmrd.Dataset(path)
         ds.write_xml_header(header)
@@ -28,11 +29,19 @@ def kspace_to_ismrmrd(kspace, header, mask, file_index, out_dir='./'):
                 )
                 ds.append_acquisition(acq)
 
-def from_fastmri_to_ismrmrd(filename, out_dir='./', accel_factor=4, split='val'):
+def from_fastmri_to_ismrmrd(filename, out_dir='./', accel_factor=4, split='val', scale_factor=1e6):
     kspace, header = _from_file_to_stuff(filename, vals=['kspace', 'ismrmrd_header'])
     file_index = Path(filename).stem
     if split == 'test':
         raise NotImplementedError('test ismrmrd generation not implemented.')
     else:
         mask = gen_mask_equidistant(kspace, accel_factor=accel_factor)
-    kspace_to_ismrmrd(kspace, header, mask, file_index, out_dir=out_dir)
+    kspace_to_ismrmrd(
+        kspace,
+        header,
+        mask,
+        file_index,
+        out_dir=out_dir,
+        accel_factor=accel_factor,
+        scale_factor=scale_factor,
+    )
