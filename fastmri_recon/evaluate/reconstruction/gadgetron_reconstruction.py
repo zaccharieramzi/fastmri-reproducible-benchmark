8c79886f50b42feeaedf868202ad5f29a4eb9358
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-15 18:04:34 +0200

    added a gadgetron grappa reconstruction file

---
+++ fastmri_recon/evaluate/reconstruction/gadgetron_reconstruction.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def gadgetron_execution(in_file, out_file, config_name, out_group):
+    subprocess.check_call([
+        'gadgetron_ismrmrd_client',
+        '-f', str(in_file),
+        '-o', str(out_file),
+        '-c', config_name,
+        '-G', out_group
+    ])
+
+def gadgetron_grappa_reconstruction(in_file, out_file):
+    gadgetron_execution(
+        in_file,
+        out_file,
+        'Generic_Cartesian_Grappa.xml',
+        'recon_grappa_generic',
+    )
