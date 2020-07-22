5a2f40752ab4e0f0cc5a7f1afdca49bffe6f912d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-22 13:57:31 +0200

    corrected oasis data dir in env config jz

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -4,6 +4,6 @@ module load python/3.7.5 cuda/10.1.2 cudnn/7.6.5.32-cuda-10.1 nccl/2.5.6-2-cuda
 conda activate fastmri-tf-2.1.0
 
 export FASTMRI_DATA_DIR=$SCRATCH/
-export OASIS_DATA_DIR=$SCRATCH/
+export OASIS_DATA_DIR=$SCRATCH/OASIS_data
 export LOGS_DIR=$SCRATCH/
 export CHECKPOINTS_DIR=$SCRATCH/
