549266e7e3aa55e6a612eed755e02d65f8cf07ee
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-22 11:05:44 +0200

    corrected gpu placement logic again and made use of the logical devices for easier debugging

--- fastmri_recon/models/utils/gpu_placement.py
+++ fastmri_recon/models/utils/gpu_placement.py
@@ -1,4 +1,4 @@
-from tensorflow.python.client import device_lib
+import tensorflow as tf
 
 
 def gpu_index_from_submodel_index(n_gpus, n_submodels, submodel_i):
@@ -7,14 +7,13 @@ def gpu_index_from_submodel_index(n_gpus, n_submodels, submodel_i):
     else:
         n_left_over_models = n_submodels % n_gpus
         n_submodels_per_gpu = n_submodels // n_gpus
-        # for i_gpu < n_left_over_models, there is n_submodels_per_gpu + 1
-        # submodels per GPU, otherwise n_submodels_per_gpu
-        i_gpu = submodel_i // (n_submodels_per_gpu + 1)
-        if i_gpu >= n_left_over_models:
-            i_gpu = submodel_i // n_submodels_per_gpu
+        i_limit_overflow = n_left_over_models * (n_submodels_per_gpu + 1)
+        if submodel_i < i_limit_overflow:
+            i_gpu = submodel_i // (n_submodels_per_gpu + 1)
+        else:
+            i_gpu = (submodel_i - n_left_over_models) // n_submodels_per_gpu
         return i_gpu
 
 def get_gpus():
-    # from https://gist.github.com/jovianlin/b5b2c4be45437992df58a7dd13dbafa7
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+    local_device_protos = tf.config.list_logical_devices('GPU')
+    return [x.name for x in local_device_protos]
