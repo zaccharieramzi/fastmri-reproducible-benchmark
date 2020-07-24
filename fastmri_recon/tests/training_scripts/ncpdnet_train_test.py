650ead8c97d794dbf350b9b8b5e26b6d6e2497fb
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-24 14:23:48 +0200

    made travis skip nc multicoil training test

--- fastmri_recon/tests/training_scripts/ncpdnet_train_test.py
+++ fastmri_recon/tests/training_scripts/ncpdnet_train_test.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from fastmri_recon.training_scripts import nc_train
@@ -8,7 +10,6 @@ from fastmri_recon.training_scripts.nc_train import train_ncpdnet
     {'n_iter': 1},
     {'n_iter': 2},
     {'dcomp': True},
-    {'multicoil': True, 'dcomp': True},
 ])
 def test_train_ncpdnet(create_full_fastmri_test_tmp_dataset, kwargs):
     nc_train.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
@@ -24,3 +25,20 @@ def test_train_ncpdnet(create_full_fastmri_test_tmp_dataset, kwargs):
         **kwargs,
     )
     # TODO: checks that the checkpoints and the logs are correctly created
+
+CI = os.environ['CONTINUOUS_INTEGRATION'] or os.environ['CI'] or os.environ['TRAVIS']
+@pytest.mark.skipif(CI)
+def test_train_ncpdnet_multicoil(create_full_fastmri_test_tmp_dataset):
+    nc_train.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
+    nc_train.LOGS_DIR = create_full_fastmri_test_tmp_dataset['logs_tmp_dir']
+    nc_train.CHECKPOINTS_DIR = create_full_fastmri_test_tmp_dataset['checkpoints_tmp_dir']
+    nc_train.n_volumes_train = 1
+    train_ncpdnet(
+        af=8,
+        n_samples=1,
+        n_epochs=1,
+        n_filters=4,
+        n_primal=2,
+        multicoil=True,
+        dcomp=True,
+    )
