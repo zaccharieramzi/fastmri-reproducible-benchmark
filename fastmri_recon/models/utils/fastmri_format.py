c722ff71b26482fa1c3fbefa2d1d676812c01f47
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-27 17:25:39 +0200

    corrected x shape in fastmri format

--- fastmri_recon/models/utils/fastmri_format.py
+++ fastmri_recon/models/utils/fastmri_format.py
@@ -26,7 +26,7 @@ def general_fastmri_format(image, output_shape=None):
     if output_shape is None:
         cropy = cropx = 320
     else:
-        cropx = tf.squeeze(output_shape[-2])
-        cropy = tf.squeeze(output_shape[-1])
+        cropx = tf.squeeze(output_shape[-1])
+        cropy = tf.squeeze(output_shape[-2])
     cropped_image = _tf_crop(abs_image, cropx=cropx, cropy=cropy)
     return cropped_image
