e0fff542527d0272f60f603c5968444f21305f0d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-27 07:48:12 -0700

    Documentation catch up (#81)
    A bit of docs for new main modules

---
+++ fastmri_recon/training_scripts/__init__.py
@@ -0,0 +1,9 @@
+"""Module with all the training scripts
+
+References
+----------
+
+..[P2020] Nicola Pezzotti et al. (2020)
+    An Adaptive Intelligence Algorithm for Undersampled Knee MRI Reconstruction: Application to the 2019 fastMRI Challenge
+    https://arxiv.org/abs/2004.07339
+"""
