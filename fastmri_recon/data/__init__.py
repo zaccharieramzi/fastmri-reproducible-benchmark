37730e29cbdb1ac4340b525bc4a52a7efebceebc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-27 11:20:33 +0200

    added non cartesian single coil dataset documentation

--- fastmri_recon/data/__init__.py
+++ fastmri_recon/data/__init__.py
@@ -3,6 +3,10 @@
 References
 ----------
 
+..[P1999] James Pipe and Padmanabhan Menon (1999)
+    Sampling density compensation in MRI: rationale and an iterative numerical solution
+    https://pubmed.ncbi.nlm.nih.gov/10025627/
+
 ..[Z2018] Jure Zbontar et al. (2018)
     fastMRI: An Open Dataset and Benchmarks for Accelerated MRI
     https://arxiv.org/abs/1811.08839
--- fastmri_recon/data/datasets/fastmri_pyfunc_non_cartesian.py
+++ fastmri_recon/data/datasets/fastmri_pyfunc_non_cartesian.py
@@ -17,7 +17,58 @@ def train_nc_kspace_dataset_from_indexable(
         compute_dcomp=False,
         **acq_kwargs,
     ):
-    """Non-cartesian dataset for the training/validation set of single coil fastMRI.
+    r"""Non-cartesian dataset for the training/validation set of single coil fastMRI.
+
+    The undersampling is performed retrospectively on the fully-sampled kspace,
+    using spiral or radial trajectories. A uniform cartesian trajectory is also
+    available for debugging.
+    The non-uniform Fourier transform is implemented by tfkbnufft.
+    The output of the dataset is of the form:
+    ```
+    (retrospectively_undersampled_nc_kspace, nc_trajectory, extra_args), ground_truth_reconstruction
+    ```
+    where `extra_args` contains the original shape fo the image, and potentially
+    the density compensation factors.
+
+    Prefetching is performed, as well as parallel calls for preprocessing when
+    rand or inner_slices are active.
+    The ground truth reconstruction is read directly from the h5 file and not
+    obtained through the Fourier inversion of the kspace.
+
+    Arguments:
+        path (str): the path to the fastMRI files. Should end with a `/`.
+        image_size (tuple of int): the fixed image size to consider for the
+            non-uniform Fourier transform. It needs to be fixed to only use
+            a single plan. An image size of (640, 400) will lead to 99.5
+            percents of the dataset being only padded and not cropped.
+        inner_slices (int or None): the slices to consider in the volumes. The
+            slicing will be performed as `inner_slices//2:-inner_slices//2`.
+            If None, all slices are considered. Defaults to None.
+        rand (bool): whether or not to sample one slice randomly from the
+            considered slices of the volumes. If None, all slices are taken.
+            Defaults to None.
+        scale_factor (int or float): the multiplicative scale factor for both the
+            kspace and the target reconstruction. Typically, 1e6 is a good value
+            for fastMRI, because it sets the values in a range acceptable for
+            neural networks training. See [R2020] (3.4 Training) for more details
+            on this value. Defaults to 1.
+        contrast (str or None): the contrast to select for this dataset. If None,
+            all contrasts are considered. Available contrasts for fastMRI single
+            coil are typically `CORPD_FBK` (Proton density) and `CORPDFS_FBK`
+            (Proton density with fat suppression). Defaults to None.
+        n_samples (int or None): the number of samples to consider from this set.
+            If None, all samples are used. Defaults to None.
+        acq_type (str): the type of non-cartesian trajectory to use. Choices are
+            `radial`, `spiral` or `cartesian` (for debugging purposes). Defaults
+            to radial.
+        compute_dcomp (bool): whether to compute and return the density compensation
+            factors. See [P1999] for more on density compensation. Defaults to
+            False.
+        **acq_kwargs: keyword arguments for the non-cartesian trajectory. See
+            fastmri_recon/data/utils/non_cartesian.py for more info.
+
+    Returns:
+        tf.data.Dataset: the training/validation non-cartesian dataset.
     """
     selection = [{'inner_slices': inner_slices, 'rand': rand}]
     def _tf_filename_to_image_and_kspace_and_contrast(filename):
