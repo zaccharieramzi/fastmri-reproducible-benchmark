a96dba0b4a8ea37f1cb839b0dee40c9684ca5f0f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-27 11:07:31 +0200

    added documentation for the single coil datasets

---
+++ fastmri_recon/data/__init__.py
@@ -0,0 +1,13 @@
+"""Data pipelines for fastMRI and OASIS datasets
+
+References
+----------
+
+..[Z2018] Jure Zbontar et al. (2018)
+    fastMRI: An Open Dataset and Benchmarks for Accelerated MRI
+    https://arxiv.org/abs/1811.08839
+
+..[R2020] Ramzi et al. (2020)
+    Benchmarking MRI Reconstruction Neural Networks on Large Public Datasets
+    https://www.mdpi.com/2076-3417/10/5/1816
+"""
