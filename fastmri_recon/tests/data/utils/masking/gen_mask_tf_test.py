eb2039069bdd34a5c6e5321906ee4eb5540714c3
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-28 15:27:24 +0200

    added equidistant mask test

--- fastmri_recon/tests/data/utils/masking/gen_mask_tf_test.py
+++ fastmri_recon/tests/data/utils/masking/gen_mask_tf_test.py
@@ -1,7 +1,7 @@
 import pytest
 import tensorflow as tf
 
-from fastmri_recon.data.utils.masking.gen_mask_tf import gen_mask_tf
+from fastmri_recon.data.utils.masking.gen_mask_tf import gen_mask_tf, gen_mask_equidistant_tf
 
 @pytest.mark.parametrize('fixed_masks', [True, False])
 @pytest.mark.parametrize('multicoil', [True, False])
@@ -14,3 +14,10 @@ def test_gen_mask_tf(fixed_masks, multicoil):
         mask_again = gen_mask_tf(kspace, accel_factor, multicoil, fixed_masks)
         tf_tester = tf.test.TestCase()
         tf_tester.assertAllEqual(mask, mask_again)
+
+@pytest.mark.parametrize('multicoil', [True, False])
+def test_gen_mask_equidistant_tf(multicoil):
+    kspace = tf.random.uniform([2, 5, 64, 32])
+    kspace = tf.cast(kspace, tf.complex64)
+    accel_factor = 2
+    gen_mask_equidistant_tf(kspace, accel_factor, multicoil)
