dcceb0b2be44598108f16eaf8f48c8d7c3aef8bc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-08-19 02:29:46 -0700

    changed submission filename for brain challenge (#91)

--- fastmri_recon/evaluate/utils/write_results.py
+++ fastmri_recon/evaluate/utils/write_results.py
@@ -6,10 +6,10 @@ import h5py
 from fastmri_recon.config import FASTMRI_DATA_DIR
 
 
-def _filename_submission(filename):
+def _filename_submission(filename, brain=False):
     relative_filename = filename.split('/')[-1]
     name = relative_filename.split('.')[0]
-    if 'v2' in name:
+    if 'v2' in name or brain:
         name = relative_filename
     else:
         name += '_v2.h5'
@@ -23,6 +23,6 @@ def write_result(exp_id, result, filename, coiltype='multicoil', scale_factor=1e
     res_path = f'{res_main_dir}{exp_id}/'
     Path(res_path).mkdir(parents=True, exist_ok=True)
     res_formatted = result[..., 0] / scale_factor
-    res_filename = _filename_submission(filename)
+    res_filename = _filename_submission(filename, brain=brain)
     with h5py.File(op.join(res_path, res_filename), 'w') as f:
         f.create_dataset('reconstruction', data=res_formatted)
