4f89a695c26ab9eea591efc6587857dc35bf952f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-08-31 11:36:45 +0200

    added compound loss to custom tf objects

--- fastmri_recon/training_scripts/custom_objects.py
+++ fastmri_recon/training_scripts/custom_objects.py
@@ -1,6 +1,9 @@
 from fastmri_recon.evaluate.metrics.tf_metrics import *
+from fastmri_recon.models.training.compile import compound_l1_mssim_loss
+
 
 CUSTOM_TF_OBJECTS = {
     'keras_psnr': keras_psnr,
     'keras_ssim': keras_ssim,
+    'compound_l1_mssim_loss': compound_l1_mssim_loss,
 }
