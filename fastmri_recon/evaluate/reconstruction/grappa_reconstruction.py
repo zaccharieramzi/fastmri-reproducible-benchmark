6ce7a87526e14675bd340a71ff23036f471bcb90
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-09-01 14:58:48 +0200

    switched to mdgrappa for fastMRI grappa recon w pygrappa

--- fastmri_recon/evaluate/reconstruction/grappa_reconstruction.py
+++ fastmri_recon/evaluate/reconstruction/grappa_reconstruction.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pygrappa import cgrappa
+from pygrappa import mdgrappa
 
 from ...data.utils.crop import crop_center
 from ...data.utils.fourier import ifft
@@ -15,7 +15,7 @@ def reco_grappa_kspace(kspace, af=4, **grappa_kwargs):
     calib = kspace[..., ctr-pd:ctr+pd].copy() # call copy()!
     recon = np.empty_like(kspace)
     for i in range(n_slices):
-        recon[i] = cgrappa(
+        recon[i] = mdgrappa(
             kspace[i].astype(np.complex),
             calib[i].astype(np.complex),
             coil_axis=0,
