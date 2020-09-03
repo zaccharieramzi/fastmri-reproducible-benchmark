723d0895ffd5928781de2834e67acca37193fff4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-09-03 12:26:34 +0200

    changed the option name in case of local config

--- fastmri_recon/evaluate/reconstruction/gadgetron_reconstruction.py
+++ fastmri_recon/evaluate/reconstruction/gadgetron_reconstruction.py
@@ -3,12 +3,15 @@ import subprocess
 
 GRAPPA_RECON_DS_NAME = 'recon_grappa_generic'
 
-def gadgetron_execution(in_file, out_file, config_name, out_group):
+def gadgetron_execution(in_file, out_file, config_name, out_group, my_config=False):
+    config_option = 'c'
+    if my_config:
+        config_option = 'C'
     subprocess.check_call([
         'gadgetron_ismrmrd_client',
         '-f', str(in_file),
         '-o', str(out_file),
-        '-c', config_name,
+        f'-{config_option}', config_name,
         '-G', out_group
     ])
 
@@ -21,4 +24,5 @@ def gadgetron_grappa_reconstruction(in_file, out_file, my_config=False):
         out_file,
         config,
         GRAPPA_RECON_DS_NAME,
+        my_config=my_config,
     )
