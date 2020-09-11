2e98c0142537067446311fca301f9c8f3ebe043a
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-09-11 11:01:24 +0200

    corrected xpdnet evaluation

--- fastmri_recon/evaluate/scripts/xpdnet_eval.py
+++ fastmri_recon/evaluate/scripts/xpdnet_eval.py
@@ -113,7 +113,7 @@ def evaluate_xpdnet(
     for x, y_true in tqdm(val_set.as_numpy_iterator(), total=n_volumes if n_samples is None else n_samples):
         y_pred = model.predict(x, batch_size=4)
         eval_res.push(y_true[..., 0], y_pred[..., 0])
-    return METRIC_FUNCS, list(m.means().values())
+    return METRIC_FUNCS, list(eval_res.means().values())
 
 @click.command()
 @click.option(
