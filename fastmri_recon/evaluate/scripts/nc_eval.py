6e1422985cf0a26b4e423ac0b6d538e5bd9dae67
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-14 18:55:37 +0200

    added possibility to evaluate non cartesian networks on 3d data

--- fastmri_recon/evaluate/scripts/nc_eval.py
+++ fastmri_recon/evaluate/scripts/nc_eval.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import click
 import tensorflow as tf
@@ -6,16 +7,19 @@ from tqdm import tqdm
 
 from fastmri_recon.config import *
 from fastmri_recon.data.datasets.fastmri_pyfunc_non_cartesian import train_nc_kspace_dataset_from_indexable as singlecoil_dataset
+from fastmri_recon.data.datasets.oasis_pyfunc_non_cartesian import train_nc_kspace_dataset_from_indexable as three_d_dataset
 from fastmri_recon.data.datasets.multicoil.fastmri_pyfunc_non_cartesian import train_nc_kspace_dataset_from_indexable as multicoil_dataset
 from fastmri_recon.evaluate.metrics.np_metrics import METRIC_FUNCS, Metrics
 from fastmri_recon.evaluate.reconstruction.non_cartesian_dcomp_reconstruction import NCDcompReconstructor
 from fastmri_recon.models.subclassed_models.ncpdnet import NCPDNet
 from fastmri_recon.models.subclassed_models.unet import UnetComplex
+from fastmri_recon.models.subclassed_models.vnet import VnetComplex
 
 
 # this number means that 99.56% of all images will not be affected by
 # cropping
 IM_SIZE = (640, 400)
+VOLUME_SIZE = (256, 256, 256)
 
 def _extract_first_elem_of_batch(inputs):
     if isinstance(inputs, (list, tuple)):
@@ -26,6 +30,7 @@ def _extract_first_elem_of_batch(inputs):
 def evaluate_nc(
         model,
         multicoil=False,
+        three_d=False,
         run_id=None,
         n_epochs=200,
         contrast=None,
@@ -37,6 +42,8 @@ def evaluate_nc(
     ):
     if multicoil:
         val_path = f'{FASTMRI_DATA_DIR}multicoil_val/'
+    elif three_d:
+        val_path = str(Path(OASIS_DATA_DIR) / 'val')
     else:
         val_path = f'{FASTMRI_DATA_DIR}singlecoil_val/'
 
@@ -44,18 +51,29 @@ def evaluate_nc(
 
     if multicoil:
         dataset = multicoil_dataset
+        image_size = IM_SIZE
+    elif three_d:
+        dataset = three_d_dataset
+        image_size = VOLUME_SIZE
     else:
         dataset = singlecoil_dataset
+        image_size = IM_SIZE
+    if not three_d:
+        add_kwargs = {
+            'contrast': contrast,
+            'rand': False,
+            'inner_slices': None,
+        }
+    else:
+        add_kwargs = {}
+    add_kwargs.update(**acq_kwargs)
     val_set = dataset(
         val_path,
-        IM_SIZE,
+        image_size,
         acq_type=acq_type,
         compute_dcomp=dcomp,
-        contrast=contrast,
-        inner_slices=None,
-        rand=False,
         scale_factor=1e6,
-        **acq_kwargs
+        **add_kwargs
     )
     if n_samples is not None:
         val_set = val_set.take(n_samples)
@@ -67,7 +85,10 @@ def evaluate_nc(
     model(inputs)
     if run_id is not None:
         model.load_weights(f'{CHECKPOINTS_DIR}checkpoints/{run_id}-{n_epochs:02d}.hdf5')
-    m = Metrics(METRIC_FUNCS)
+    if three_d:
+        m = Metrics({'PSNR': METRIC_FUNCS['PSNR']})
+    else:
+        m = Metrics(METRIC_FUNCS)
     for x, y_true in tqdm(val_set.as_numpy_iterator(), total=199 if n_samples is None else n_samples):
         y_pred = model.predict(x, batch_size=1)
         m.push(y_true[..., 0], y_pred[..., 0])
@@ -75,6 +96,7 @@ def evaluate_nc(
 
 def evaluate_ncpdnet(
         multicoil=False,
+        three_d=False,
         dcomp=False,
         normalize_image=False,
         n_iter=10,
@@ -84,13 +106,18 @@ def evaluate_ncpdnet(
         refine_smaps=True,
         **eval_kwargs
     ):
+    if three_d:
+        image_size = VOLUME_SIZE
+    else:
+        image_size = IM_SIZE
     run_params = {
         'n_primal': n_primal,
         'multicoil': multicoil,
+        'three_d': three_d,
         'activation': non_linearity,
         'n_iter': n_iter,
         'n_filters': n_filters,
-        'im_size': IM_SIZE,
+        'im_size': image_size,
         'dcomp': dcomp,
         'normalize_image': normalize_image,
         'refine_smaps': refine_smaps,
@@ -101,6 +128,7 @@ def evaluate_ncpdnet(
         model,
         multicoil=multicoil,
         dcomp=dcomp,
+        three_d=three_d,
         **eval_kwargs,
     )
 
@@ -137,6 +165,32 @@ def evaluate_unet(
     return evaluate_nc(
         model,
         multicoil=multicoil,
+        dcomp=dcomp,
+        **eval_kwargs,
+    )
+
+def evaluate_vnet(
+        n_layers=4,
+        dcomp=False,
+        base_n_filters=16,
+        non_linearity='relu',
+        **eval_kwargs
+    ):
+    run_params = {
+        'non_linearity': non_linearity,
+        'n_layers': n_layers,
+        'layers_n_channels': [base_n_filters * 2**i for i in range(n_layers)],
+        'layers_n_non_lins': 2,
+        'res': True,
+        'im_size': VOLUME_SIZE,
+        'dcomp': dcomp,
+        'dealiasing_nc': True,
+    }
+
+    model = VnetComplex(**run_params)
+    return evaluate_nc(
+        model,
+        three_d=True,
         dcomp=dcomp,
         **eval_kwargs,
     )
@@ -197,6 +251,18 @@ def evaluate_unet(
     default=None,
     help='The number of samples to use for evaluation.'
 )
+@click.option(
+    'three_d',
+    '-3d',
+    is_flag=True,
+    help='Whether you want to use 3d data.'
+)
+@click.option(
+    'dcomp',
+    '-dc',
+    is_flag=True,
+    help='Whether you want to use density compensation.'
+)
 def evaluate_nc_click(
         af,
         run_id,
@@ -206,15 +272,22 @@ def evaluate_nc_click(
         acq_type,
         n_epochs,
         n_samples,
+        three_d,
+        dcomp
     ):
     if model == 'pdnet':
         evaluate_function = evaluate_ncpdnet
         add_kwargs = {'refine_smaps': refine_smaps}
     elif model == 'unet':
-        evaluate_function = evaluate_unet
+        if three_d:
+            evaluate_function = evaluate_vnet
+        else:
+            evaluate_function = evaluate_unet
         add_kwargs = {}
     if multicoil:
         add_kwargs.update(dcomp=True)
+    else:
+        add_kwargs.update(dcomp=dcomp)
     metric_names, metrics = evaluate_function(
         af=af,
         run_id=run_id,
@@ -222,6 +295,7 @@ def evaluate_nc_click(
         acq_type=acq_type,
         n_epochs=n_epochs,
         n_samples=n_samples,
+        three_d=three_d,
         **add_kwargs,
     )
     print(metric_names)
