4871605cd0d2e78a3efdc4f8c5c865063573a4aa
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-15 11:05:14 +0200

    added printing mechanism within nc eval script

--- fastmri_recon/evaluate/scripts/nc_eval.py
+++ fastmri_recon/evaluate/scripts/nc_eval.py
@@ -92,6 +92,8 @@ def evaluate_nc(
     for x, y_true in tqdm(val_set.as_numpy_iterator(), total=199 if n_samples is None else n_samples):
         y_pred = model.predict(x, batch_size=1)
         m.push(y_true[..., 0], y_pred[..., 0])
+    print(METRIC_FUNCS.keys())
+    print(list(m.means().values()))
     return METRIC_FUNCS, list(m.means().values())
 
 def evaluate_ncpdnet(
