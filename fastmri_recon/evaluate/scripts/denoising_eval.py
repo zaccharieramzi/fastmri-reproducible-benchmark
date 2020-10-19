086a12f6f372b87ed6000b6bd046ec824acb96f7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-19 10:56:14 +0200

    corrected evaluation with stds

--- fastmri_recon/evaluate/scripts/denoising_eval.py
+++ fastmri_recon/evaluate/scripts/denoising_eval.py
@@ -32,29 +32,11 @@ def evaluate_xpdnet_denoising(
     if n_samples is not None:
         val_set = val_set.take(n_samples)
 
-
-    def tf_psnr(y_true, y_pred):
-        perm_psnr = [3, 1, 2, 0]
-        psnr = tf.image.psnr(
-            tf.transpose(y_true, perm_psnr),
-            tf.transpose(y_pred, perm_psnr),
-            tf.reduce_max(y_true),
-        )
-        return psnr
-    def tf_ssim(y_true, y_pred):
-        perm_ssim = [0, 1, 2, 3]
-        ssim = tf.image.ssim(
-            tf.transpose(y_true, perm_ssim),
-            tf.transpose(y_pred, perm_ssim),
-            tf.reduce_max(y_true),
-        )
-        return ssim
     if isinstance(model, tuple):
         model = build_model_from_specs(*model)
-    model.compile(loss=tf_psnr, metrics=[tf_ssim])
     model.load_weights(f'{CHECKPOINTS_DIR}checkpoints/{run_id}-{n_epochs:02d}.hdf5')
     eval_res = Metrics(METRIC_FUNCS)
     for x, y_true in tqdm(val_set.as_numpy_iterator(), total=n_volumes if n_samples is None else n_samples):
         y_pred = model.predict(x)
         eval_res.push(y_true[..., 0], y_pred[..., 0])
-    return METRIC_FUNCS, zip(list(eval_res.means().values()), list(eval_res.stddevs().values()))
+    return METRIC_FUNCS, (list(eval_res.means().values()), list(eval_res.stddevs().values()))
