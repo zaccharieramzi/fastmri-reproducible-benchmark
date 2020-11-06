2a511f43d7610b0ec3df7fa2ca59f07e558715f9
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-06 14:06:42 +0100

    New denoising (#103)
    Removed old tfio files
    Added new tf-fastmri-data for noisy datasets

--- fastmri_recon/evaluate/scripts/denoising_eval.py
+++ fastmri_recon/evaluate/scripts/denoising_eval.py
@@ -1,11 +1,15 @@
 import os
 
 import tensorflow as tf
+from tf_fastmri_data.datasets.noisy import NoisyFastMRIDatasetBuilder
+from tqdm import tqdm
 
 from fastmri_recon.config import *
-from fastmri_recon.data.datasets.fastmri_pyfunc_denoising import train_noisy_dataset_from_indexable
+from fastmri_recon.evaluate.metrics.np_metrics import Metrics, METRIC_FUNCS
 from fastmri_recon.models.subclassed_models.denoisers.proposed_params import build_model_from_specs
 
+
+tf.random.set_seed(0)
 
 def evaluate_xpdnet_denoising(
         model,
@@ -13,45 +17,27 @@ def evaluate_xpdnet_denoising(
         n_epochs=200,
         contrast='CORPD_FBK',
         noise_std=30,
-        n_samples=None,
+        n_samples=100,
         cuda_visible_devices='0123',
     ):
-
-    val_path = f'{FASTMRI_DATA_DIR}singlecoil_val/'
-
     os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(cuda_visible_devices)
 
-    val_set = train_noisy_dataset_from_indexable(
-        val_path,
-        noise_std=noise_std,
+    val_set = NoisyFastMRIDatasetBuilder(
+        dataset='val',
+        noise_power_spec=noise_std,
+        noise_input=False,
         contrast=contrast,
-        inner_slices=None,
-        rand=True,
-        scale_factor=1e6,
-    )
-    if n_samples is not None:
-        val_set = val_set.take(n_samples)
+        slice_random=True,
+        scale_factor=1e4,
+        force_determinism=True,
+    ).preprocessed_ds
+    val_set = val_set.take(n_samples)
 
-
-    def tf_psnr(y_true, y_pred):
-        perm_psnr = [3, 1, 2, 0]
-        psnr = tf.image.psnr(
-            tf.transpose(y_true, perm_psnr),
-            tf.transpose(y_pred, perm_psnr),
-            tf.reduce_max(y_true),
-        )
-        return psnr
-    def tf_ssim(y_true, y_pred):
-        perm_ssim = [0, 1, 2, 3]
-        ssim = tf.image.ssim(
-            tf.transpose(y_true, perm_ssim),
-            tf.transpose(y_pred, perm_ssim),
-            tf.reduce_max(y_true),
-        )
-        return ssim
     if isinstance(model, tuple):
         model = build_model_from_specs(*model)
-    model.compile(loss=tf_psnr, metrics=[tf_ssim])
     model.load_weights(f'{CHECKPOINTS_DIR}checkpoints/{run_id}-{n_epochs:02d}.hdf5')
-    eval_res = model.evaluate(val_set, verbose=1, steps=199 if n_samples is None else None)
-    return model.metrics_names, eval_res
+    eval_res = Metrics(METRIC_FUNCS)
+    for x, y_true in tqdm(val_set.as_numpy_iterator(), total=n_samples):
+        y_pred = model.predict(x)
+        eval_res.push(y_true[..., 0], y_pred[..., 0])
+    return METRIC_FUNCS, (list(eval_res.means().values()), list(eval_res.stddevs().values()))
--- fastmri_recon/tests/training_scripts/denoising/generic_train_test.py
+++ fastmri_recon/tests/training_scripts/denoising/generic_train_test.py
@@ -1,13 +1,16 @@
 import time
 
-from fastmri_recon import config
+from tf_fastmri_data import config as config_data
+
+from fastmri_recon import config as config_output
 from fastmri_recon.models.subclassed_models.denoisers.proposed_params import get_model_specs
 
 
 def test_train_denoiser(create_full_fastmri_test_tmp_dataset):
-    config.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
-    config.LOGS_DIR = create_full_fastmri_test_tmp_dataset['logs_tmp_dir']
-    config.CHECKPOINTS_DIR = create_full_fastmri_test_tmp_dataset['checkpoints_tmp_dir']
+    config_data.FASTMRI_DATA_DIR = create_full_fastmri_test_tmp_dataset['fastmri_tmp_data_dir']
+    config_data.PATHS_MAP[False][False]['train'] = 'singlecoil_train/singlecoil_train'
+    config_output.LOGS_DIR = create_full_fastmri_test_tmp_dataset['logs_tmp_dir']
+    config_output.CHECKPOINTS_DIR = create_full_fastmri_test_tmp_dataset['checkpoints_tmp_dir']
     from fastmri_recon.training_scripts.denoising.generic_train import train_denoiser
     model_specs = get_model_specs(force_res=True)
     model_specs = [ms for ms in model_specs if ms[1] == 'small']
