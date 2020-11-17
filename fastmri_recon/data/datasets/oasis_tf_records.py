6b400796b875f07671e6d4cf497208286c1e8ba7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-17 13:51:19 +0100

    created a oasis tf records dataset

---
+++ fastmri_recon/data/datasets/oasis_tf_records.py
@@ -0,0 +1,32 @@
+from functools import partial
+from pathlib import Path
+
+import tensorflow as tf
+
+from fastmri_recon.data.utils.tfrecords import decode_example, get_extension_for_acq
+
+
+def train_nc_kspace_dataset_from_tfrecords(
+        path,
+        volume_size=(256, 256, 256),
+        scale_factor=1,
+        acq_type='radial_stacks',
+        compute_dcomp=False,
+        **acq_kwargs,
+    ):
+    pattern = get_extension_for_acq(
+        volume_size=volume_size,
+        scale_factor=scale_factor,
+        acq_type=acq_type,
+        **acq_kwargs,
+    )
+    filenames = sorted(list(Path(path).glob(f'*{pattern}.tfrecords')))
+    raw_dataset = tf.data.TFRecordDataset(
+        filenames,
+        num_parallel_reads=tf.data.experimental.AUTOTUNE,
+    )
+    volume_ds = raw_dataset.map(
+        partial(decode_example, compute_dcomp=compute_dcomp),
+        num_parallel_reads=3,
+    ).repeat().prefetch(buffer_size=3)
+    return volume_ds
