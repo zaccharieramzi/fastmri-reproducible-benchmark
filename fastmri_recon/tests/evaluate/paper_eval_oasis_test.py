d09c372b69a1ca5d5765963e1fac4cfbdc410570
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-23 10:45:39 +0100

    corrected the eval tests

--- fastmri_recon/tests/evaluate/paper_eval_oasis_test.py
+++ fastmri_recon/tests/evaluate/paper_eval_oasis_test.py
@@ -1,6 +1,6 @@
 from fastmri_recon.evaluate.scripts.paper_eval_oasis import evaluate_paper_oasis
 
 
-def evaluate_paper_oasis_test():
+def test_evaluate_paper_oasis():
     # TODO: add a test on the metrics values
     evaluate_paper_oasis(n_samples=2)
