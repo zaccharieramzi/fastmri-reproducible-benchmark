3f13a97add34186e6ee4d06c21f872618d294ab5
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-08 17:43:29 +0100

    added an adjusting utility to make sure all images are the same size

--- fastmri_recon/data/utils/crop.py
+++ fastmri_recon/data/utils/crop.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+
+
 def crop_center(img, cropx, cropy=None):
     # taken from https://stackoverflow.com/questions/39382412/crop-center-portion-of-a-numpy-image/39382475
     if cropy is None:
@@ -6,3 +9,20 @@ def crop_center(img, cropx, cropy=None):
     startx = x//2 - (cropx//2)
     starty = y//2 - (cropy//2)
     return img[..., starty:starty+cropy, startx:startx+cropx]
+
+def adjust_image_size(image, target_image_size, multicoil=False):
+    height = tf.shape(image)[-2]
+    width = tf.shape(image)[-1]
+    n_slices = tf.shape(image)[0]
+    reshaped_image = tf.reshape(image, [height, width, -1])
+    padded_image = tf.image.resize_image_with_crop_or_pad(
+        reshaped_image,
+        target_image_size[0],
+        target_image_size[1],
+    )
+    if multicoil:
+        final_shape = [n_slices, height, width]
+    else:
+        final_shape = [n_slices, -1, height, width]
+    reshaped_padded_image = tf.reshape(padded_image, final_shape)
+    return reshaped_padded_image
