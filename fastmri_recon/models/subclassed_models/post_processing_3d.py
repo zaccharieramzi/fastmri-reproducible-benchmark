fe3b49f2eb2565be2ca15204a5d916e8be1f20a6
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-16 12:59:00 +0100

    corrected padding in post processing model

--- fastmri_recon/models/subclassed_models/post_processing_3d.py
+++ fastmri_recon/models/subclassed_models/post_processing_3d.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.models import Model
 
 from fastmri_recon.models.subclassed_models.vnet import Vnet
+from fastmri_recon.models.utils.pad_for_pool import pad_for_pool_whole_plane
 
 
 class PostProcessVnet(Model):
@@ -19,7 +20,14 @@ class PostProcessVnet(Model):
 
     def predict_batched(self, inputs, batch_size=1):
         outputs = self.recon_model.predict(inputs, batch_size=batch_size)
+        outputs, paddings = pad_for_pool_whole_plane(outputs, self.postproc_model.n_layers)
         outputs = outputs[None]
         outputs = self.postproc_model.predict_on_batch(outputs)
         outputs = outputs[0]
+        outputs = outputs[
+            :,
+            paddings[0][0]:tf.shape(outputs)[1] - paddings[0][1],
+            paddings[1][0]:tf.shape(outputs)[1] - paddings[1][1],
+            :,
+        ]
         return outputs
