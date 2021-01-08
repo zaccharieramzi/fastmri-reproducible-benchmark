6560c296134d1ae380cfd2ef2a6b1eb7b86f7b5d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-01-08 12:20:30 +0100

    added conv to the custom objects

--- fastmri_recon/training_scripts/custom_objects.py
+++ fastmri_recon/training_scripts/custom_objects.py
@@ -2,6 +2,7 @@ from functools import partial
 
 from fastmri_recon.evaluate.metrics.tf_metrics import *
 from fastmri_recon.models.training.compile import compound_l1_mssim_loss
+from fastmri_recon.models.subclassed_models.vnet import Conv
 
 mssim = partial(compound_l1_mssim_loss, alpha=0.9999)
 mssim.__name__ = "mssim"
@@ -10,4 +11,5 @@ CUSTOM_TF_OBJECTS = {
     'keras_ssim': keras_ssim,
     'compound_l1_mssim_loss': compound_l1_mssim_loss,
     'mssim': mssim,
+    'Conv': Conv,
 }
