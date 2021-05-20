8dd72a242bec8503a257315dcd8febf51fc4298a
chaithyagr <chaithyagr@gitlab.com>
2021-05-20 10:18:18 +0200

    Update model saving workaround

--- fastmri_recon/training_scripts/model_saving_workaround.py
+++ fastmri_recon/training_scripts/model_saving_workaround.py
@@ -1,9 +1,30 @@
+import os
+import pickle as pkl
+
 from tensorflow.keras.callbacks import ModelCheckpoint
+import tensorflow.keras.backend as K
+
 
 # taken from https://github.com/tensorflow/tensorflow/issues/39679
-class ModelCheckpointWorkAround(ModelCheckpoint):
+class ModelCheckpointWithOptimizer(ModelCheckpoint):
+    def __init__(self, filepath, save_optimizer=True, **kwargs):
+        os.makedirs(os.path.dirname(filepath))
+        self.save_optimizer = save_optimizer
+        super(ModelCheckpointWithOptimizer, self).__init__(filepath=filepath, **kwargs)
+
     def set_model(self, model):
         # Work around, so that the if at
         # https://github.com/tensorflow/tensorflow/blob/1186e3f2098793952aa82bf356dfe51b967fb26c/tensorflow/python/keras/callbacks.py#L1189
         # is skipped, so that self.save_weights_only remains False.
         self.model = model
+
+    def _save_model(self, epoch, logs):
+        # Save the model with super
+        super(ModelCheckpointWithOptimizer, self)._save_model(epoch, logs)
+        if self.save_optimizer:
+            # Save the optimizer
+            folder = os.path.dirname(self._get_file_path(epoch, logs))
+            symbolic_weights = getattr(self.model.optimizer, 'weights')
+            weight_values = K.batch_get_value(symbolic_weights)
+            with open(os.path.join(folder, 'optimizer.pkl'), 'wb') as f:
+                pkl.dump(weight_values, f)
