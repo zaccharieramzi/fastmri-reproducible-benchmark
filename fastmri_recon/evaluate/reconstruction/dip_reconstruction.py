4c0635e4d45967a1e7114cfce4320f89fe997ece
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-14 10:22:37 +0200

    corrected reconstruction to work in batch

--- fastmri_recon/evaluate/reconstruction/dip_reconstruction.py
+++ fastmri_recon/evaluate/reconstruction/dip_reconstruction.py
@@ -26,13 +26,20 @@ def reconstruct_dip(
     if model_checkpoint is not None:
         model.load_weights(model_checkpoint)
     default_model_compile(model, lr=lr, loss=dip_loss)
-    in_random_vector = tf.random.normal([1, in_dim])
-    _history = model.fit(
-        x=[in_random_vector, ktraj],
-        y=kspace,
-        epochs=n_iter,
-    )
-    if save_model:
-        model.save_weights(save_path)
-    output = model.generate(in_random_vector, fastmri_format=True)
+    n_slices = kspace.shape[0]
+    outputs = []
+    for i_slice in range(n_slices):
+        if i_slice > 0 and model_checkpoint is None:
+            n_iter = n_iter // 10
+        in_random_vector = tf.random.normal([1, in_dim])
+        _history = model.fit(
+            x=[in_random_vector, ktraj[i_slice:i_slice+1]],
+            y=kspace[i_slice:i_slice+1],
+            epochs=n_iter,
+        )
+        if save_model:
+            model.save_weights(save_path)
+        output = model.generate(in_random_vector, fastmri_format=True)
+        outputs.append(output)
+    output = tf.concat(outputs, axis=0)
     return output
