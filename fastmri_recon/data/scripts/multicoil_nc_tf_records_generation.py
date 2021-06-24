2f0366b49bfd459c7bf72808f0eacd5319f498e9
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-24 11:10:31 +0200

    corrected dataset building for ncmc tf records gen

--- fastmri_recon/data/scripts/multicoil_nc_tf_records_generation.py
+++ fastmri_recon/data/scripts/multicoil_nc_tf_records_generation.py
@@ -104,10 +104,10 @@ def generate_multicoil_nc_tf_records(
             filename,
             selection=selection,
         )
-        data = tf.data.Dataset.zip([
+        data = tf.data.Dataset.zip((
             tf.data.Dataset.from_tensor_slices(image),
             tf.data.Dataset.from_tensor_slices(kspace),
-        ])
+        ))
         data = data.batch(len(image))
         options = tf.data.Options()
         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
