3aa34f1c762d31f1f7e83259c5c87b0a24773802
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-24 15:51:52 +0200

    added psnr/ssim on figures

--- fastmri_recon/evaluate/utils/save_figure.py
+++ fastmri_recon/evaluate/utils/save_figure.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from fastmri_recon.config import LOGS_DIR
+from fastmri_recon.evaluate.metrics.np_metrics import psnr, ssim
 
 
 plt.rcParams['figure.figsize'] = (5, 5)
@@ -14,16 +15,31 @@ def save_figure(im_recos, img_batch, name, slice_index=15, acq_type='radial', af
     else:
         im_reco = im_recos[0]
         im_gt = img_batch[0]
+    im_gt = im_gt.squeeze()
+    im_reco = im_reco.squeeze()
     im_res = np.abs(im_gt - im_reco)
+    im_res = np.abs(im_res)
+    im_gt = np.abs(im_gt)
+    im_reco = np.abs(im_reco)
+    p = psnr(im_gt, im_reco)
+    s = ssim(im_gt[None], im_reco[None])
     fig, ax = plt.subplots(1, frameon=False)
-    ax.imshow(np.abs(np.squeeze(im_reco)), aspect='auto')
+    ax.imshow(im_reco, aspect='auto')
+    ax.text(
+        1.0, 1.0,
+        f'PSNR: {p:.2f}/ SSIM: {s:.4f}',
+        ha='left', va='top',
+        fontsize='medium',
+        color='white',
+        transform=fig.transFigure,
+    )
     ax.axis('off')
     fig.savefig(f'{LOGS_DIR}figures/{name}{acq_type}_recon_af{af}.png')
     fig, ax = plt.subplots(1, frameon=False)
-    ax.imshow(np.abs(np.squeeze(im_res)), aspect='auto')
+    ax.imshow(im_res, aspect='auto')
     ax.axis('off')
     fig.savefig(f'{LOGS_DIR}figures/{name}{acq_type}_residu_af{af}.png')
     fig, ax = plt.subplots(1)
-    ax.imshow(np.abs(np.squeeze(im_gt)))
+    ax.imshow(im_gt)
     ax.axis('off')
     fig.savefig(f'{LOGS_DIR}figures/image_gt.png')
