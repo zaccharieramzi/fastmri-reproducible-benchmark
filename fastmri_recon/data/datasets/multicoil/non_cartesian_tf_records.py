75b98d8545c97a5f2b216cd0b9fe0bc641d8ed4d
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-06-29 11:12:46 +0200

    Several improvements on results presentation (#136)

--- fastmri_recon/data/datasets/multicoil/non_cartesian_tf_records.py
+++ fastmri_recon/data/datasets/multicoil/non_cartesian_tf_records.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import tensorflow as tf
 
 from fastmri_recon.data.utils.tfrecords import decode_ncmc_example
+from fastmri_recon.data.utils.h5 import from_file_to_contrast
 
 
 def train_nc_kspace_dataset_from_tfrecords(
@@ -20,11 +21,14 @@ def train_nc_kspace_dataset_from_tfrecords(
         brain=False,
     ):
     pattern = acq_type
-    filenames = sorted(list(Path(path).glob(f'*{pattern}.tfrecords')))
+    if contrast is None and af == 4:
+        filenames = sorted(list(Path(path).glob(f'*{pattern}.tfrecords')))
+    else:
+        filenames = get_tfrecords_files_for_contrast(path, contrast, pattern, af)
     raw_dataset = tf.data.TFRecordDataset(
         [str(f) for f in filenames],
         num_parallel_reads=2 if rand else None,
-    )
+    ).apply(tf.data.experimental.ignore_errors(log_warning=True))
     if n_samples is not None:
         raw_dataset.take(n_samples)
     volume_ds = raw_dataset.map(
@@ -34,3 +38,15 @@ def train_nc_kspace_dataset_from_tfrecords(
     if rand:
         volume_ds = volume_ds.repeat().prefetch(buffer_size=2)
     return volume_ds
+
+def get_tfrecords_files_for_contrast(path, contrast, pattern='radial', af=4):
+    if af == 8:
+        pattern = pattern + '_af8'
+    tfrec_filenames = sorted(list(Path(path).glob(f'*{pattern}.tfrecords')))
+    h5_filenames = sorted(list(Path(path).glob(f'*.h5')))
+    assert len(tfrec_filenames) == len(h5_filenames)
+    filtered_tfrec_filenames = [
+        tfrec_filename for (tfrec_filename, h5_filename) in zip(tfrec_filenames, h5_filenames)
+        if from_file_to_contrast(h5_filename) == contrast
+    ]
+    return filtered_tfrec_filenames
