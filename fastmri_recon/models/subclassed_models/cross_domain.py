correcting normalization in cross domain

fastmri_recon/models/subclassed_models/cross_domain.py
@@ -277,11 +277,12 @@ class CrossDomainNet(Model):
             normalization_factor = tf.reduce_max(
                 tf.abs(image),
                 axis=[1, 2, 3, 4] if self.multicoil else [1, 2, 3],
+                keepdims=True,
             )
             normalization_factor = tf.cast(normalization_factor, image.dtype)
             orig_image_shape = image.shape
             orig_kspace_shape = original_kspace.shape
-            image = image / normalization_factor[:, None]
+            image = image / normalization_factor
             image.set_shape(orig_image_shape)
             original_kspace = original_kspace / normalization_factor
             original_kspace.set_shape(orig_kspace_shape)